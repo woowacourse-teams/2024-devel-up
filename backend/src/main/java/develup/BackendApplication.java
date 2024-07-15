12eb7a7e545d4dfa924ef70ceaec4c606511af9a
le2sky <leehaneul990623@gmail.com>
2024-07-15 17:20:21 +0900

    chore: 백엔드 초기 설정

---
+++ backend/src/main/java/develup/BackendApplication.java
@@ -0,0 +1,12 @@
+package develup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
