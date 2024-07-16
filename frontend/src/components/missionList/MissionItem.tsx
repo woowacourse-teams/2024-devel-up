39634a51c0361cb761bb4026137f4a8bbfd8cc32
hanyoung <phanyoung@naver.com>
2024-07-16 10:45:27 +0900

    refactor: MissionItem 아이템 클릭 시 navigate 대신 link 사용

--- frontend/src/components/missionList/MissionItem.tsx
+++ frontend/src/components/missionList/MissionItem.tsx
@@ -1,5 +1,5 @@
 import * as S from './MissionList.styled';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import type { Mission } from './missionMocks';
 
 interface MissionItemProps {
@@ -7,29 +7,25 @@ interface MissionItemProps {
 }
 
 export default function MissionItem({ mission }: MissionItemProps) {
-  const navigate = useNavigate();
-
   const { id, title, thumbnail } = mission;
 
-  const handleMissionClick = () => {
-    navigate(`/missions/${id}`);
-  };
-
   return (
-    <S.MissionItemContainer onClick={handleMissionClick}>
-      <S.MissionThumbnailImg src={thumbnail} alt={title} />
+    <Link to={`/missions/${id}`}>
+      <S.MissionItemContainer>
+        <S.MissionThumbnailImg src={thumbnail} alt={title} />
 
-      <S.MissionDescription>
-        <S.MissionTitle>[미션] {title}</S.MissionTitle>
+        <S.MissionDescription>
+          <S.MissionTitle>[미션] {title}</S.MissionTitle>
 
-        <S.TagWrapper>
-          <S.PopularTag>🔥 인기 미션</S.PopularTag>
-          <S.BackendTag>☕️ JAVA</S.BackendTag>
-          <S.InsuranceTag>🔒 리뷰 100% 보장</S.InsuranceTag>
-        </S.TagWrapper>
-        <S.HorizontalLine />
-        <S.MissionPrice>무료</S.MissionPrice>
-      </S.MissionDescription>
-    </S.MissionItemContainer>
+          <S.TagWrapper>
+            <S.PopularTag>🔥 인기 미션</S.PopularTag>
+            <S.BackendTag>☕️ JAVA</S.BackendTag>
+            <S.InsuranceTag>🔒 리뷰 100% 보장</S.InsuranceTag>
+          </S.TagWrapper>
+          <S.HorizontalLine />
+          <S.MissionPrice>무료</S.MissionPrice>
+        </S.MissionDescription>
+      </S.MissionItemContainer>
+    </Link>
   );
 }
