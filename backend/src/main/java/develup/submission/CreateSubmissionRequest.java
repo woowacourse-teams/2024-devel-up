0d4f6b5c74b11947849b4883fa77566ccdd3e14d
Minjoo <m0522inju@gmail.com>
2024-07-17 17:08:11 +0900

    feat: CreateSubmissionRequest entity 변환 로직 구현

--- backend/src/main/java/develup/submission/CreateSubmissionRequest.java
+++ backend/src/main/java/develup/submission/CreateSubmissionRequest.java
@@ -1,8 +1,15 @@
 package develup.submission;
 
+import develup.member.Member;
+import develup.mission.Mission;
+
 record CreateSubmissionRequest(
         Long missionId,
         String url,
         String comment
 ) {
+
+    public Submission toEntity(Member member, Mission mission) {
+        return new Submission(url, comment, member, mission);
+    }
 }
