d7ca6553604bf1592e1ad73ea0691b09a354587e
Minjoo <m0522inju@gmail.com>
2024-07-17 17:07:43 +0900

    feat: SubmissionResponse DTO 구현

---
+++ backend/src/main/java/develup/submission/SubmissionResponse.java
@@ -0,0 +1,20 @@
+package develup.submission;
+
+public record SubmissionResponse(
+        Long id,
+        Long memberId,
+        Long missionId,
+        String url,
+        String comment
+) {
+
+    public static SubmissionResponse of(Submission submission) {
+        return new SubmissionResponse(
+                submission.getId(),
+                submission.getMember().getId(),
+                submission.getMission().getId(),
+                submission.getUrl(),
+                submission.getComment()
+        );
+    }
+}
