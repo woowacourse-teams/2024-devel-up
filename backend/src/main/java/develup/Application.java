5aa19a80588d0739edaf541195430bd03362a655
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-18 00:00:30 +0900

    feat: 환경 변수 설정

--- backend/src/main/java/develup/Application.java
+++ backend/src/main/java/develup/Application.java
@@ -2,8 +2,10 @@ package develup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
