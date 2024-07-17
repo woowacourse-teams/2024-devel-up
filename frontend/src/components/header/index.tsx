2443dcb533c624fb90c46013ada7be922fdc49f4
hanyoung <phanyoung@naver.com>
2024-07-17 16:51:05 +0900

    feat: header 알림 아이콘 추가

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom';
 import * as S from './Header.styled';
 import { ROUTES } from '@/constants/routes';
+import { useState } from 'react';
 
 export default function Header() {
+  const [, setIsNotiModalOpen] = useState(false);
+
+  const handleBellClick = () => {
+    setIsNotiModalOpen((prev) => !prev);
+  };
+
   return (
     <>
       <S.Container>
-        <Link to={ROUTES.main}>
-          <S.Logo>🚀 Devel Up</S.Logo>
-        </Link>
+        <S.LeftSection>
+          <Link to={ROUTES.main}>
+            <S.Logo>🚀 Devel Up</S.Logo>
+          </Link>
+        </S.LeftSection>
+        <S.RightSection>
+          <S.BellIcon onClick={handleBellClick} />
+        </S.RightSection>
       </S.Container>
       <S.Spacer />
     </>
