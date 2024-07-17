45680cc50e3618353464bc2ba6ab47dab6d2ad14
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-18 00:03:59 +0900

    feat: 멤버를 찾고 없을 시 생성하는 코드 작성

---
+++ backend/src/main/java/develup/member/MemberService.java
@@ -0,0 +1,21 @@
+package develup.member;
+
+import develup.auth.SocialProfile;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public MemberResponse findOrCreateMember(SocialProfile profile, Provider provider) {
+        Member member = memberRepository.findBySocialIdAndProvider(profile.id(), provider)
+                .orElseGet(() -> memberRepository.save(profile.toMember(provider)));
+
+        return MemberResponse.from(member);
+    }
+}
