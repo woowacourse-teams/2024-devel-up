c6062b7340cc7c38eb87f994c6005bf1006e756a
Minjoo <m0522inju@gmail.com>
2024-07-17 17:45:30 +0900

    feat: 미션 제출 API 구현

---
+++ backend/src/main/java/develup/submission/SubmissionApi.java
@@ -0,0 +1,27 @@
+package develup.submission;
+
+import java.net.URI;
+import develup.member.Member;
+import develup.support.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class SubmissionApi {
+
+    private final SubmissionService submissionService;
+
+    public SubmissionApi(SubmissionService submissionService) {
+        this.submissionService = submissionService;
+    }
+
+    @PostMapping("/submissions")
+    public ResponseEntity<ApiResponse<SubmissionResponse>> postSubmission(@RequestBody CreateSubmissionRequest request) {
+        Member member = new Member(1L);
+        SubmissionResponse newSubmission = submissionService.submit(member, request);
+        return ResponseEntity.created(URI.create("/submissions/" + newSubmission.id()))
+                .body(new ApiResponse<>(newSubmission));
+    }
+}
