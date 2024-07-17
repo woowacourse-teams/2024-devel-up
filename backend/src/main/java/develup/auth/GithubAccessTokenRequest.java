e372f28fff3e17c8e363fb8faf905aba4fe623d0
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-18 00:03:09 +0900

    feat: github oauth client 요청 코드 작성
    - github에 access token을 요청하는 코드
    - github에 user info를 요청하는 코드

---
+++ backend/src/main/java/develup/auth/GithubAccessTokenRequest.java
@@ -0,0 +1,14 @@
+package develup.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GithubAccessTokenRequest(
+        String code,
+
+        @JsonProperty("client_id")
+        String clientId,
+
+        @JsonProperty("client_secret")
+        String clientSecret
+) {
+}
