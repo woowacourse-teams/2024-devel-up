bcffa0cb133f319a665b176b81b263c8ad59a708
bmj0053 <bmj05378@gmail.com>
2024-07-18 13:44:00 +0900

    design: 완료한 미션 컴포넌트 구현

--- frontend/src/components/MyMission/MyMissionCompleted/MyMissionCompleted.tsx
+++ frontend/src/components/MyMission/MyMissionCompleted/MyMissionCompleted.tsx
@@ -1,3 +1,94 @@
+import * as S from './MyMissionCompleted.styled';
+
+const mocks = [
+  {
+    id: 1,
+    mission: {
+      id: 1,
+      title: '루터회관 흡연 단속',
+      language: 'JAVA',
+      description: '루터회관 흡연 벌칙 프로그램을 구현한다.',
+      thumbnail:
+        'https://dszw1qtcnsa5e.cloudfront.net/community/20231115/ccefd368-2687-4d26-954d-712315f38fea/2232611625864AF59FF189AB61A75869.jpeg',
+      url: 'https://github.com/develup-mission/java-smoking',
+    },
+    myPrLink: 'https://github.com/develup-mission/java-smoking/pull/1',
+    pairPrLink: 'https://github.com/develup-mission/java-smoking/pull/2',
+    status: '리뷰 완료',
+  },
+  {
+    id: 2,
+    mission: {
+      id: 2,
+      title: '루터회관 흡연 단속 2',
+      language: 'JAVA',
+      description: '루터회관 흡연 벌칙 프로그램을 구현한다.',
+      thumbnail:
+        'https://dszw1qtcnsa5e.cloudfront.net/community/20231115/ccefd368-2687-4d26-954d-712315f38fea/2232611625864AF59FF189AB61A75869.jpeg',
+      url: 'https://github.com/develup-mission/java-smoking',
+    },
+    myPrLink: 'https://github.com/develup-mission/java-smoking/pull/1',
+    pairPrLink: 'https://github.com/develup-mission/java-smoking/pull/2',
+    status: '리뷰 완료',
+  },
+  {
+    id: 3,
+    mission: {
+      id: 2,
+      title: '루터회관 흡연 단속 3',
+      language: 'JAVA',
+      description: '루터회관 흡연 벌칙 프로그램을 구현한다.',
+      thumbnail:
+        'https://dszw1qtcnsa5e.cloudfront.net/community/20231115/ccefd368-2687-4d26-954d-712315f38fea/2232611625864AF59FF189AB61A75869.jpeg',
+      url: 'https://github.com/develup-mission/java-smoking',
+    },
+    myPrLink: 'https://github.com/develup-mission/java-smoking/pull/1',
+    pairPrLink: 'https://github.com/develup-mission/java-smoking/pull/2',
+    status: '리뷰 완료',
+  },
+  {
+    id: 4,
+    mission: {
+      id: 2,
+      title: '루터회관 흡연 단속 4',
+      language: 'JAVA',
+      description: '루터회관 흡연 벌칙 프로그램을 구현한다.',
+      thumbnail:
+        'https://dszw1qtcnsa5e.cloudfront.net/community/20231115/ccefd368-2687-4d26-954d-712315f38fea/2232611625864AF59FF189AB61A75869.jpeg',
+      url: 'https://github.com/develup-mission/java-smoking',
+    },
+    myPrLink: 'https://github.com/develup-mission/java-smoking/pull/1',
+    pairPrLink: 'https://github.com/develup-mission/java-smoking/pull/2',
+    status: '리뷰 완료',
+  },
+];
+
 export default function MyMissionCompleted() {
-  return <div>완료한 미션</div>;
+  return (
+    <S.MyMissionCompletedContainer>
+      <S.Title>완료한 미션</S.Title>
+      <S.MissionCardListWrapper>
+        {mocks.map((mock) => (
+          <S.MissionCardWrapper key={mock.id}>
+            <S.MissionCardHeaderWrapper>
+              <S.ThumbnailImg src={mock.mission.thumbnail} />
+              <S.MissionLanguageBox>{mock.mission.language}</S.MissionLanguageBox>
+            </S.MissionCardHeaderWrapper>
+
+            <S.MissionCardContentWrapper>
+              <div>
+                <S.MissionTitle>{mock.mission.title}</S.MissionTitle>
+                <S.MissionDate>2024.07.17 ~ 2024.07.24</S.MissionDate>
+              </div>
+
+              <S.PrButtonWrapper>
+                <S.PrButton>페어 PR 이동</S.PrButton>
+                <S.PrButton>내 PR 이동</S.PrButton>
+              </S.PrButtonWrapper>
+            </S.MissionCardContentWrapper>
+          </S.MissionCardWrapper>
+        ))}
+      </S.MissionCardListWrapper>
+    </S.MyMissionCompletedContainer>
+  );
 }
--- frontend/src/pages/MyMissionPage.styled.ts
+++ frontend/src/pages/MyMissionPage.styled.ts
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 
 export const MyMissionPageContainer = styled.div`
   width: 100rem;
-  margin: 0 auto;
+  margin: 4rem auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 7rem;
 `;
