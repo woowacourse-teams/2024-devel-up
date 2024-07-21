13331c9f5ce25c16cfcbf88673d683b95781392b
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-21 23:22:55 +0900

    test: 비로그인, 제출한 멤버, 제출하지 않은 멤버에 따라 다른 단건 조회 테스트

--- backend/src/test/java/develup/mission/MissionServiceTest.java
+++ backend/src/test/java/develup/mission/MissionServiceTest.java
@@ -4,6 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import develup.member.Member;
+import develup.member.MemberRepository;
+import develup.member.Provider;
+import develup.submission.Submission;
+import develup.submission.SubmissionRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,11 @@ class MissionServiceTest {
     @Autowired
     private MissionRepository missionRepository;
 
+    @Autowired
+    private SubmissionRepository submissionRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
     @DisplayName("모든 미션을 조회한다.")
     void getMissions() {
@@ -32,19 +42,51 @@ class MissionServiceTest {
     }
 
     @Test
-    @DisplayName("미션 식별자로 미션 단건을 조회한다.")
-    void getMissionById() {
+    @DisplayName("로그인하지 않은 사용자가 미션 식별자로 미션 단건을 조회한다.")
+    void getMissionByIdWhenGuest() {
         Mission mission = missionRepository.save(new Mission("미션 1", Language.JAVA, "미션 설명", "미션 썸네일", "미션 url"));
+        Member member = createMember(-1L);
 
-        MissionResponse response = missionService.getMissionById(mission.getId());
+        MissionResponse response = missionService.getMissionById(mission.getId(), member, true);
 
         assertThat(response).isEqualTo(MissionResponse.from(mission));
     }
 
     @Test
+    @DisplayName("로그인한 멤버가 제출하지 않은 미션을 미션 식별자로 미션 단건을 조회한다.")
+    void getMissionByIdWhenNotSubmitted() {
+        Mission mission = missionRepository.save(new Mission("미션 1", Language.JAVA, "미션 설명", "미션 썸네일", "미션 url"));
+        Member member = memberRepository.save(createMember(1L));
+
+        MissionResponse response = missionService.getMissionById(mission.getId(), member, false);
+
+        assertThat(response).isEqualTo(MissionResponse.from(mission));
+    }
+
+    @Test
+    @DisplayName("로그인한 멤버가 제출한 미션을 미션 식별자로 미션 단건을 조회한다.")
+    void getMissionByIdWhenSubmitted() {
+        Mission mission = missionRepository.save(new Mission("미션 1", Language.JAVA, "미션 설명", "미션 썸네일", "미션 url"));
+        Member member = memberRepository.save(createMember(1L));
+        String submittedPrUrl = "https://gitbub.com/example/pr";
+        submissionRepository.save(new Submission(submittedPrUrl, "comment", member, mission));
+
+        MissionResponse response = missionService.getMissionById(mission.getId(), member, false);
+
+        assertThat(response).isEqualTo(MissionResponse.from(mission, true, submittedPrUrl));
+    }
+
+    @Test
     @DisplayName("존재하지 않는 미션 식별자로 미션 조회시 예외가 발생한다.")
     void getMissionByUndefinedId() {
-        assertThatThrownBy(() -> missionService.getMissionById(-1L))
-                .isInstanceOf(IllegalArgumentException.class);
+        Member member = createMember(-1L);
+
+        assertThatThrownBy(() -> missionService.getMissionById(-1L, member, true))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 미션입니다.");
+    }
+
+    private Member createMember(Long id) {
+        return new Member(id, "email", Provider.GITHUB, 1234L, "name", "image");
     }
 }
