6693f7855b29df0a20f20d34f3cace78d6190419
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-23 21:03:50 +0900

    feat: AuthorizationExtractor 인터페이스 및 구현체 작성

--- backend/src/main/java/develup/application/auth/AuthorizationExtractor.java
+++ backend/src/main/java/develup/application/auth/AuthorizationExtractor.java
@@ -1,18 +1,17 @@
 package develup.application.auth;
 
-import java.util.Optional;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.NativeWebRequest;
 
 public interface AuthorizationExtractor<T> {
 
-    Optional<T> extract(HttpServletRequest request);
+    T extract(HttpServletRequest request);
 
-    default Optional<T> extract(NativeWebRequest request) {
+    default T extract(NativeWebRequest request) {
         HttpServletRequest httpServletRequest = request.getNativeRequest(HttpServletRequest.class);
 
         if (httpServletRequest == null) {
-            return Optional.empty();
+            return null;
         }
 
         return extract(httpServletRequest);
--- backend/src/main/java/develup/infra/auth/CookieAuthorizationExtractor.java
+++ backend/src/main/java/develup/infra/auth/CookieAuthorizationExtractor.java
@@ -1,26 +1,28 @@
 package develup.infra.auth;
 
 import java.util.Arrays;
-import java.util.Optional;
 import develup.application.auth.AuthorizationExtractor;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CookieAuthorizationExtractor implements AuthorizationExtractor<String> {
 
     private static final String TOKEN_COOKIE_NAME = "token";
 
     @Override
-    public Optional<String> extract(HttpServletRequest request) {
+    public String extract(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
 
         if (cookies == null) {
-            return Optional.empty();
+            return null;
         }
 
         return Arrays.stream(cookies)
                 .filter(cookie -> TOKEN_COOKIE_NAME.equals(cookie.getName()))
                 .map(Cookie::getValue)
-                .findFirst();
+                .findFirst()
+                .orElse(null);
     }
 }
