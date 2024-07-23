7f1e75dec63dc1887345c91e2716031d687c9d0b
hanyoung <phanyoung@naver.com>
2024-07-23 15:01:03 +0900

    fix: getPlaceholderImg 하드코딩된 코드 수정

--- frontend/src/components/common/Card/getPlaceholderImg.ts
+++ frontend/src/components/common/Card/getPlaceholderImg.ts
@@ -1,5 +1,5 @@
 const getPlaceholderImg = (width: number, height: number, text?: string) => {
-  return `https://placehold.co/600x400?text=${text ? text : `${width} X ${height}`}`;
+  return `https://placehold.co/${width}x${height}?text=${text ? text : `${width} X ${height}`}`;
 };
 
 export default getPlaceholderImg;
