61fa5e0455d02277e4840b402dc576118e44a492
hanyoung <phanyoung@naver.com>
2024-07-23 13:28:44 +0900

    feat: getPlaceholderImg default text 수정

--- frontend/src/components/common/Card/getPlaceholderImg.ts
+++ frontend/src/components/common/Card/getPlaceholderImg.ts
@@ -1,5 +1,5 @@
 const getPlaceholderImg = (width: number, height: number, text?: string) => {
-  return `https://placehold.co/600x400?text=${text ? text : `${width}+${height}`}`;
+  return `https://placehold.co/600x400?text=${text ? text : `${width} X ${height}`}`;
 };
 
 export default getPlaceholderImg;
