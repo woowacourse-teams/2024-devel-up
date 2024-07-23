fd890522fd3ae24d78b95765a77507f8b8a7b993
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-23 14:08:57 +0900

    refactor: config 파일 패키지화

--- backend/src/main/java/develup/support/config/CorsConfig.java
+++ backend/src/main/java/develup/support/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package develup.support;
+package develup.support.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
