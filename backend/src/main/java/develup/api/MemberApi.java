85cb6fdcd57b88179b8329b7cfbc091656c414ca
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-23 21:12:15 +0900

    feat: 내 정보 조회 기능 구현

---
+++ backend/src/main/java/develup/api/MemberApi.java
@@ -0,0 +1,22 @@
+package develup.api;
+
+import develup.application.auth.Accessor;
+import develup.application.member.MemberResponse;
+import develup.application.member.MemberService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberApi {
+
+    private final MemberService memberService;
+
+    public MemberApi(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/member/mine")
+    public MemberResponse getMyInfo(@Auth Accessor accessor) {
+        return memberService.getMemberById(accessor.id());
+    }
+}
