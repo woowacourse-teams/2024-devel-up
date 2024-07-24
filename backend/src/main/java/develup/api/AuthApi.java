9cf6ef61a2f5c3c439ecf542f499b0840af94dd0
yoonseo choi <choiyoonseo02@gmail.com>
2024-07-24 11:09:04 +0900

    refactor: 성능 측정을 위한 백엔드 패키지 수정

--- backend/src/main/java/develup/api/AuthApi.java
+++ backend/src/main/java/develup/api/AuthApi.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthApi {
 
+    //
     private final GithubOAuthService githubOAuthService;
     private final MemberService memberService;
     private final AuthService authService;
