9941e44c5a5b2bb400489c75e50f5368a8d8dfa7
김민수 MINSOO KIM <alstn113@gmail.com>
2024-07-24 10:40:11 +0900

    refactor: 예외 메세지 변경

--- backend/src/main/java/develup/api/exception/ExceptionType.java
+++ backend/src/main/java/develup/api/exception/ExceptionType.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public enum ExceptionType {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않은 회원입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다."),
     SAME_SUBMISSION_PAIR(HttpStatus.BAD_REQUEST, "같은 제출끼리 페어가 될 수 없습니다."),
     MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 미션입니다."),
