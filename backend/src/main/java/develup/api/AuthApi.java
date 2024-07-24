9442fdb832cf430c61d9da12b2ab9750237388c8
yoonseo choi <choiyoonseo02@gmail.com>
2024-07-24 11:23:13 +0900

    refactor: 테스트를 위한 주석 원복

--- backend/src/main/java/develup/api/AuthApi.java
+++ backend/src/main/java/develup/api/AuthApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthApi {
-//테스트
+
     private final GithubOAuthService githubOAuthService;
     private final MemberService memberService;
     private final AuthService authService;
