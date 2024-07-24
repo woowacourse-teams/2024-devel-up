55f60dfae5f79fe05daee69d6f91abc06317d6d9
yoonseo choi <choiyoonseo02@gmail.com>
2024-07-24 16:24:51 +0900

    refactor: pr assignee 를 sender 로 변경

--- backend/src/main/java/develup/api/AuthApi.java
+++ backend/src/main/java/develup/api/AuthApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthApi {
-    //test
+    //test1
     private final GithubOAuthService githubOAuthService;
     private final MemberService memberService;
     private final AuthService authService;
