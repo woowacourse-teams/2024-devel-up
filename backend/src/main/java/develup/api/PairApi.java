6dc238e32638860695caa216d7496d10d8705cb1
Haneul Lee <leehaneul990623@gmail.com>
2024-07-25 16:33:36 +0900

    백엔드 인가 적용(issue #140) (#151)
    * refactor: submission api 인가 적용
    
    * refactor: mission 단건 조회 인가 적용
    
    * docs: api 오퍼레이션 작성
    
    * chore: auth 어노테이션 히든 처리

--- backend/src/main/java/develup/api/PairApi.java
+++ backend/src/main/java/develup/api/PairApi.java
@@ -4,12 +4,14 @@ import develup.api.common.ApiResponse;
 import develup.application.submission.MyMissionResponse;
 import develup.application.submission.PairService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "페어 API")
 public class PairApi {
 
     private final PairService pairService;
@@ -19,7 +21,7 @@ public class PairApi {
     }
 
     @PostMapping("/pair-review/{submissionId}")
-    @Operation(summary = "코드리뷰를 완료한다.", description = "코드 리뷰를 완료한 뒤 변경된 페어 매칭 정보를 반환한다.")
+    @Operation(summary = "코드 리뷰 완료 API", description = "코드 리뷰를 완료한 뒤 변경된 페어 매칭 정보를 반환한다.")
     public ResponseEntity<ApiResponse<MyMissionResponse>> review(@PathVariable Long submissionId) {
         return ResponseEntity.ok(new ApiResponse<>(pairService.reviewComplete(submissionId)));
     }
