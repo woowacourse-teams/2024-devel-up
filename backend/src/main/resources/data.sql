6681ca9433fea592748af1e30b0bc7c8688531e2
Minjoo <m0522inju@gmail.com>
2024-08-06 14:54:00 +0900

    chore: 미션 데이터 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,3 +6,7 @@ VALUES ('test1@gmail.com', 'GITHUB', '1234', '리브', 'www.naver.com');
 
 INSERT INTO member (email, provider, social_id, name, image_url)
 VALUES ('test1@gmail.com', 'GITHUB', '1234', '아톰', 'www.naver.com');
+
+INSERT INTO mission (title, thumbnail, url)
+VALUES ('java-guessing-number', 'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-smoking.png',
+        'https://github.com/develup-mission/java-guessing-number')
