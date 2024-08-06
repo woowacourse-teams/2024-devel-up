675342eb41ceea916256571ca9553c49c9176b34
brgndy <brgndy96@gmail.com>
2024-08-06 15:34:04 +0900

    refactor: 공통 모달 wrapper으로 분리

--- frontend/src/components/PopUp/PopUpContent.tsx
+++ frontend/src/components/PopUp/PopUpContent.tsx
@@ -1,5 +1,5 @@
 import * as S from './SubmitSuccessPopUp.styled';
-import Modal from '../common/Modal/Modal';
+import ModalWrapper from '../common/Modal/ModalWrapper';
 
 interface PopUpContentProps {
   onClick: () => void;
@@ -8,8 +8,8 @@ interface PopUpContentProps {
 export default function PopUpContent({ onClick }: PopUpContentProps) {
   return (
     <S.SubmitPopUpContainer>
-      <Modal.SubTitle>미션 제출에 성공했어요!</Modal.SubTitle>
-      <Modal.Title>페어 매칭에 성공하면 알려드릴게요!</Modal.Title>
+      <ModalWrapper.SubTitle>미션 제출에 성공했어요!</ModalWrapper.SubTitle>
+      <ModalWrapper.Title>페어 매칭에 성공하면 알려드릴게요!</ModalWrapper.Title>
       <S.MissionImgWrapper>
         {/* 서버에서 이미지를 제대로 못받아오고 있어서 일단 임시 이미지로 대체합니다. */}
         <S.MissionImg
