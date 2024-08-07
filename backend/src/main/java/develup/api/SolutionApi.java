9e003821279b386f1a3e521cc41caf81f396c878
le2sky <leehaneul990623@gmail.com>
2024-08-08 08:03:52 +0900

    feat: 누락된 솔루션 api tag 추가

--- backend/src/main/java/develup/api/SolutionApi.java
+++ backend/src/main/java/develup/api/SolutionApi.java
@@ -7,12 +7,14 @@ import develup.domain.solution.Solution;
 import develup.domain.solution.SolutionRepository;
 import develup.domain.solution.SolutionSummary;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "솔루션 API")
 public class SolutionApi {
 
     private final SolutionService solutionService;
