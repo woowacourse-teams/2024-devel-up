37855905ee31da0007baf1e23709ae1e3f4b5bb7
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-07 10:54:42 +0900

    feat: mission api swagger 적용

--- backend/src/main/java/develup/api/MissionApi.java
+++ backend/src/main/java/develup/api/MissionApi.java
@@ -7,12 +7,15 @@ import develup.application.auth.Accessor;
 import develup.application.mission.MissionResponse;
 import develup.application.mission.MissionService;
 import develup.application.mission.MissionWithStartedResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "미션 API")
 public class MissionApi {
 
     private final MissionService missionService;
@@ -22,6 +25,7 @@ public class MissionApi {
     }
 
     @GetMapping("/missions")
+    @Operation(summary = "미션 목록 조회 API", description = "미션 목록을 조회합니다.")
     public ResponseEntity<ApiResponse<List<MissionResponse>>> getMissions() {
         List<MissionResponse> responses = missionService.getMissions();
 
@@ -29,6 +33,10 @@ public class MissionApi {
     }
 
     @GetMapping("/missions/{missionId}")
+    @Operation(
+            summary = "미션 조회 API",
+            description = "미션을 조회합니다. 비로그인, 로그인 사용자 중 제출 여부에 따라 시작 상태 응답이 달라집니다."
+    )
     public ResponseEntity<ApiResponse<MissionWithStartedResponse>> getMission(
             @PathVariable Long missionId,
             @Auth(required = false) Accessor accessor
