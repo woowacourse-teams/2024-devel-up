aa8539a9488e3e8d5e70ad7b18f52bb679aeab03
yoonseo choi <choiyoonseo02@gmail.com>
2024-08-08 17:41:31 +0900

    refactor: sql 대문자로 변경

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,9 +12,9 @@ VALUES ('루터회관 흡연 단속', 'https://raw.githubusercontent.com/develup
 INSERT INTO mission (title, thumbnail, url)
 VALUES ('java-guessing-number', 'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-smoking.png','https://github.com/develup-mission/java-guessing-number');
 
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (1, 1, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (1, 2, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (1, 3, '라이언 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (2, 1, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (2, 2, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) values (2, 3, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 1, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 2, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 3, '라이언 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 1, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 2, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 3, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
