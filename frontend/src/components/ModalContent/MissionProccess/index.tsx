86ba4976f49a19f5a68d622e5ea8885eab23ec76
brgndy <brgndy96@gmail.com>
2024-08-08 13:44:48 +0900

    refactor: 실제 API와 연동

--- frontend/src/components/ModalContent/MissionProccess/index.tsx
+++ frontend/src/components/ModalContent/MissionProccess/index.tsx
@@ -3,6 +3,8 @@ import * as S from './ModalProcess.styled';
 import LeftArrow from '@/assets/images/smallLeftArrow.svg';
 import RightArrow from '@/assets/images/smallRightArrow.svg';
 import ContentImage from '@/assets/images/contentImage.svg';
+import Button from '@/components/common/Button/Button';
+import { GithubIcon } from '@/components/MissionSubmit/SubmitBanner.styled';
 
 const CONTENT_LIST = [
   {
@@ -10,7 +12,9 @@ const CONTENT_LIST = [
     image: '',
     content: '미션 저장소로 이동해주세요',
   },
-  { id: 2, image: '', content: '저쩌고 어쩌고' },
+  { id: 2, image: '', content: '미션을 포크해주세요' },
+  { id: 3, image: '', content: '미션을 진행해주세요' },
+  { id: 4, image: '', content: '미션을 제출해주세요' },
 ];
 
 interface MissionProcessProps {
@@ -20,13 +24,14 @@ interface MissionProcessProps {
 export default function MissionProcess({ handleModalClose }: MissionProcessProps) {
   const [contentId, setContentId] = useState(1);
   const currentContent = CONTENT_LIST.find((content) => content.id === contentId);
+  const isEndContent = contentId === CONTENT_LIST.length;
 
   const handleNextMissionProcess = () => {
-    setContentId((prev) => prev + 1);
+    setContentId((prev) => Math.min(prev + 1, CONTENT_LIST.length));
   };
 
   const handlePreviousMissionProcess = () => {
-    setContentId((prev) => prev - 1);
+    setContentId((prev) => Math.max(prev - 1, 1));
   };
 
   return (
@@ -39,14 +44,24 @@ export default function MissionProcess({ handleModalClose }: MissionProcessProps
       <S.Text>{currentContent?.content}</S.Text>{' '}
       {/* TODO 현재 공통 컴포넌트 설정에는 아이콘 위치 같은게 고정이 되어있어서 일단 기본 태그로 구현해놓습니다 @버건디 */}
       <S.ButtonWrapper>
-        <S.Button onClick={handlePreviousMissionProcess}>
-          <LeftArrow />
-          Prev
-        </S.Button>
-        <S.RightButton onClick={handleNextMissionProcess}>
-          Next
-          <RightArrow />
-        </S.RightButton>
+        {isEndContent ? (
+          <>
+            <Button type="icon" content="미션 코드 보러 가기">
+              <GithubIcon />
+            </Button>
+          </>
+        ) : (
+          <>
+            <S.Button onClick={handlePreviousMissionProcess}>
+              <LeftArrow />
+              Prev
+            </S.Button>
+            <S.RightButton onClick={handleNextMissionProcess}>
+              Next
+              <RightArrow />
+            </S.RightButton>
+          </>
+        )}
       </S.ButtonWrapper>
     </S.MissionProcessContentContainer>
   );
--- frontend/src/pages/MissionDetailPage.tsx
+++ frontend/src/pages/MissionDetailPage.tsx
@@ -4,12 +4,10 @@ import MissionDetailHeader from '@/components/MissionDetail/MissionDetailHeader'
 import { useParams } from 'react-router-dom';
 import * as S from './MissionDetailPage.styled';
 import useMission from '@/hooks/useMission';
-import MOCK_MISSION_WITH_STARTED from '@/mocks/missionWithStarted.json';
 
 export default function MissionDetailPage() {
   const { id } = useParams();
-  const missionData = MOCK_MISSION_WITH_STARTED[0];
-  // const { data: missionData } = useMission(Number(id));
+  const { data: missionData } = useMission(Number(id));
 
   return (
     <S.MissionDetailPageContainer>
