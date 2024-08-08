34fa82935820f92d4ef8025bca08f38cf7d11e11
박한영(Ryan) <phanyoung@naver.com>
2024-08-08 16:22:22 +0900

    Solution 리스트 페이지 리팩토링 및 스타일링 보완 (issue #224) (#236)
    * feat: solution 리스트 스타일링 개선
    
    * feat: 미션 리스트 페이지 추가 및 헤더 수정

--- frontend/src/apis/solutions.ts
+++ frontend/src/apis/solutions.ts
@@ -1,6 +1,24 @@
-import { develupAPIClient } from './clients/develupClient';
-import { PATH } from './paths';
+import { develupAPIClient } from '@/apis/clients/develupClient';
+import { PATH } from '@/apis/paths';
 import type { Solution } from '@/types/solution';
+
+export interface SolutionSummary {
+  // solution 리스트에 필요한 필드만 포함한 데이터 (solution 원본 데이터와는 다름)
+  id: number;
+  thumbnail: string;
+  description: string;
+  title: string;
+}
+
+interface GetSolutionSummariesResponse {
+  data: SolutionSummary[];
+}
+
+export const getSolutionSummaries = async (): Promise<SolutionSummary[]> => {
+  const { data } = await develupAPIClient.get<GetSolutionSummariesResponse>(PATH.solutionSummaries);
+
+  return data;
+};
 
 export interface PostSolutionResponse {
   data: Solution;
---
+++ frontend/src/pages/MainPage/MainPage.styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MainPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  margin: 0 auto;
+  margin-bottom: 10rem;
+  padding-top: 6rem;
+  width: fit-content;
+`;
+
+export const MissionListTitle = styled.h2`
+  font-size: 2.4rem;
+  font-weight: 700;
+  margin-bottom: 3rem;
+`;
