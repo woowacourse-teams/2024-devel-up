c80502ebdb2d050ffe1151e29bf269bc1274911d
le2sky <leehaneul990623@gmail.com>
2024-08-11 21:46:47 +0900

    test: api test support 구현

---
+++ backend/src/test/java/develup/api/ApiTestSupport.java
@@ -0,0 +1,42 @@
+package develup.api;
+
+import develup.api.auth.AuthArgumentResolver;
+import develup.api.auth.CookieAuthorizationExtractor;
+import develup.application.auth.AuthService;
+import develup.application.member.MemberService;
+import develup.application.mission.MissionService;
+import develup.application.solution.SolutionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = {
+        AuthApi.class,
+        MemberApi.class,
+        MissionApi.class,
+        SolutionApi.class
+})
+public class ApiTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    protected AuthService authService;
+
+    @MockBean
+    protected MemberService memberService;
+
+    @MockBean
+    protected MissionService missionService;
+
+    @MockBean
+    protected SolutionService solutionService;
+
+    @MockBean
+    protected CookieAuthorizationExtractor cookieAuthorizationExtractor;
+
+    @MockBean
+    protected AuthArgumentResolver argumentResolver;
+}
