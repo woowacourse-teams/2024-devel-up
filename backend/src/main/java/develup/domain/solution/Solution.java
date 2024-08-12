c14a35a24c8174b6ff15af852e3a6d9b609f586b
yoonseo choi <choiyoonseo02@gmail.com>
2024-08-12 16:26:27 +0900

    refactor: Solution column에 nullable false 추가

--- backend/src/main/java/develup/domain/solution/Solution.java
+++ backend/src/main/java/develup/domain/solution/Solution.java
@@ -13,6 +13,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -22,9 +23,11 @@ public class Solution {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JoinColumn(nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Mission mission;
 
+    @JoinColumn(nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
