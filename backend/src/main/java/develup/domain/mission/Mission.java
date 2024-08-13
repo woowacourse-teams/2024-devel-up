4a6d1bcd460c56ce354155f1ab68fa2fe76ca19a
le2sky <leehaneul990623@gmail.com>
2024-08-13 13:53:25 +0900

    feat: 미션 해시 태그 엔티티 연결

--- backend/src/main/java/develup/domain/mission/Mission.java
+++ backend/src/main/java/develup/domain/mission/Mission.java
@@ -1,10 +1,18 @@
 package develup.domain.mission;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import develup.api.exception.DevelupException;
+import develup.api.exception.ExceptionType;
+import develup.domain.hashtag.HashTag;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Mission {
@@ -28,19 +36,63 @@ public class Mission {
     @Column(nullable = false)
     private String url;
 
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.PERSIST)
+    private List<MissionHashTag> hashTags = new ArrayList<>();
+
     protected Mission() {
     }
 
-    public Mission(String title, String thumbnail, String summary, String url) {
-        this(null, title, thumbnail, summary, url);
+    public Mission(String title, String thumbnail, String summary, String url, List<HashTag> hashTags) {
+        this(null, title, thumbnail, summary, url, hashTags);
     }
 
-    public Mission(Long id, String title, String thumbnail, String summary, String url) {
+    public Mission(Long id, String title, String thumbnail,String summary, String url, List<HashTag> hashTags) {
         this.id = id;
         this.title = title;
         this.thumbnail = thumbnail;
         this.summary = summary;
         this.url = url;
+        this.hashTags.addAll(mapToMissionHashTag(hashTags));
+    }
+
+    public void tagAll(List<HashTag> tags) {
+        validateDuplicated(tags);
+        validateAlreadyTagged(tags);
+
+        hashTags.addAll(mapToMissionHashTag(tags));
+    }
+
+    private void validateAlreadyTagged(List<HashTag> tags) {
+        boolean alreadyTagged = hashTags.stream()
+                .map(MissionHashTag::getHashTag)
+                .anyMatch(tags::contains);
+
+        if (alreadyTagged) {
+            throw new DevelupException(ExceptionType.DUPLICATED_HASHTAG);
+        }
+    }
+
+    private void validateDuplicated(List<HashTag> tags) {
+        int uniqueSize = tags.stream()
+                .distinct()
+                .toList()
+                .size();
+
+        if (uniqueSize != tags.size()) {
+            throw new DevelupException(ExceptionType.DUPLICATED_HASHTAG);
+        }
+    }
+
+    private List<MissionHashTag> mapToMissionHashTag(List<HashTag> tags) {
+        return tags.stream()
+                .map(it -> new MissionHashTag(this, it))
+                .toList();
+    }
+
+    public String getDescriptionUrl() {
+        String[] split = url.split("/");
+
+        return DESCRIPTION_BASE_URL_PREFIX + split[split.length - 1] + DESCRIPTION_BASE_URL_SUFFIX;
     }
 
     public Long getId() {
@@ -63,9 +115,7 @@ public class Mission {
         return url;
     }
 
-    public String getDescriptionUrl() {
-        String[] split = url.split("/");
-
-        return DESCRIPTION_BASE_URL_PREFIX + split[split.length - 1] + DESCRIPTION_BASE_URL_SUFFIX;
+    public List<MissionHashTag> getHashTags() {
+        return Collections.unmodifiableList(hashTags);
     }
 }
---
+++ backend/src/main/java/develup/domain/mission/MissionHashTag.java
@@ -0,0 +1,50 @@
+package develup.domain.mission;
+
+import develup.domain.hashtag.HashTag;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class MissionHashTag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Mission mission;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private HashTag hashTag;
+
+    protected MissionHashTag() {
+    }
+
+    public MissionHashTag(Mission mission, HashTag hashTag) {
+        this(null, mission, hashTag);
+    }
+
+    public MissionHashTag(Long id, Mission mission, HashTag hashTag) {
+        this.id = id;
+        this.mission = mission;
+        this.hashTag = hashTag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+
+    public HashTag getHashTag() {
+        return hashTag;
+    }
+}
