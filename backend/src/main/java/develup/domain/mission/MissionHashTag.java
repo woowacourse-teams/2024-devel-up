546ef06aa515ee1253804ff26fc7db2ee1c72926
le2sky <leehaneul990623@gmail.com>
2024-08-13 14:28:00 +0900

    refactor: mission hash tag 접근 제어자 변경

--- backend/src/main/java/develup/domain/mission/MissionHashTag.java
+++ backend/src/main/java/develup/domain/mission/MissionHashTag.java
@@ -9,7 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class MissionHashTag {
+class MissionHashTag {
 
     @Id
     @GeneratedValue
--- backend/src/main/java/develup/domain/mission/MissionRepository.java
+++ backend/src/main/java/develup/domain/mission/MissionRepository.java
@@ -1,10 +1,7 @@
 package develup.domain.mission;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
 import java.util.Optional;
->>>>>>> e601a20 (feat: 해시태그 존재 미션 단건 조회 기능 구현)
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
