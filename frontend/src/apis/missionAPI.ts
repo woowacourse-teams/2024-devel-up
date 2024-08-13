85a50ac8afff014f7a8fb51a79ff8aa163365c87
bmj0053 <bmj05378@gmail.com>
2024-08-13 10:39:28 +0900

    remove: 미션 현황 페이지 관련 코드 제거

--- frontend/src/apis/missionAPI.ts
+++ frontend/src/apis/missionAPI.ts
@@ -1,14 +1,6 @@
 import { develupAPIClient } from './clients/develupClient';
 import { PATH } from './paths';
-import type { Mission, MissionSubmission, SubmissionPayload, Submission } from '@/types';
-
-interface getMissionInProgressResponse {
-  data: MissionSubmission;
-}
-
-interface getMissionCompletedResponse {
-  data: MissionSubmission[];
-}
+import type { Mission, SubmissionPayload, Submission } from '@/types';
 
 interface getMissionByIdResponse {
   data: Mission;
@@ -26,30 +18,6 @@ export const getAllMissions = async (): Promise<Mission[]> => {
 
 export const getMissionById = async (id: number): Promise<Mission> => {
   const { data } = await develupAPIClient.get<getMissionByIdResponse>(`${PATH.missionList}/${id}`);
-
-  return data;
-};
-
-export const getMissionInProgress = async (): Promise<MissionSubmission> => {
-  const { data } = await develupAPIClient.get<getMissionInProgressResponse>(
-    PATH.submissionsInProgress,
-  );
-
-  return data;
-};
-
-export const getMissionCompleted = async (): Promise<MissionSubmission[]> => {
-  const { data } = await develupAPIClient.get<getMissionCompletedResponse>(PATH.submissions);
-
-  return data;
-};
-
-// getMissionInProgressResponse 타입 네이밍 변경 예정입니다 @프룬
-// 해당 타입이 미션 현황 페이지에서 쓰이는 api 반환값으로 많이 쓰이고 있음
-export const postCompleteReview = async (submissionId: number): Promise<MissionSubmission> => {
-  const { data } = await develupAPIClient.post<getMissionInProgressResponse>(
-    `${PATH.pairReview}/${submissionId}`,
-  );
 
   return data;
 };
--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,12 +1,10 @@
 export const ROUTES = {
   main: '/',
-  submission: '/submission',
   missionDetail: '/missions',
   missionList: '/missions',
   profile: '/profile',
   guide: '/guide',
   submitSolution: '/submit/solution',
-  submissions: '/submissions',
   error: '/error',
   solutions: '/solutions',
 } as const;
