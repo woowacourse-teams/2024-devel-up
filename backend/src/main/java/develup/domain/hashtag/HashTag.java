252ad703ec7036980f105776d069e75e40b6806e
le2sky <leehaneul990623@gmail.com>
2024-08-13 17:47:53 +0900

    fix: generated value identity 사용하도록 변경

--- backend/src/main/java/develup/domain/hashtag/HashTag.java
+++ backend/src/main/java/develup/domain/hashtag/HashTag.java
@@ -4,13 +4,14 @@ import java.util.Objects;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class HashTag {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- backend/src/main/java/develup/domain/mission/MissionHashTag.java
+++ backend/src/main/java/develup/domain/mission/MissionHashTag.java
@@ -4,15 +4,16 @@ import develup.domain.hashtag.HashTag;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-class MissionHashTag {
+public class MissionHashTag {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
