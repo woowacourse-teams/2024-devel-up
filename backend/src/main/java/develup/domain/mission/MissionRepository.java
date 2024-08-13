ca1a3365e1ab5105b707d9a97ec932b8c87abb60
le2sky <leehaneul990623@gmail.com>
2024-08-13 14:27:43 +0900

    feat: 해시태그 존재 미션 단건 조회 기능 구현

--- backend/src/main/java/develup/domain/mission/MissionRepository.java
+++ backend/src/main/java/develup/domain/mission/MissionRepository.java
@@ -1,6 +1,10 @@
 package develup.domain.mission;
 
+<<<<<<< HEAD
 import java.util.List;
+=======
+import java.util.Optional;
+>>>>>>> e601a20 (feat: 해시태그 존재 미션 단건 조회 기능 구현)
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +12,12 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     @Query("SELECT m.url FROM Mission m")
     List<String> findUrl();
+
+    @Query("""
+            SELECT DISTINCT m
+            FROM Mission m
+            JOIN FETCH m.hashTags ht
+            WHERE m.id = :id
+            """)
+    Optional<Mission> findWithHashTagsById(Long id);
 }
