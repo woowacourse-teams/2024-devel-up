3514798bc75974c16769d491f3135a1ab0da448e
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-14 11:31:09 +0900

    chore: 솔루션 댓글 초기 데이터 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,13 +8,45 @@ INSERT INTO member (email, provider, social_id, name, image_url)
 VALUES ('test1@gmail.com', 'GITHUB', '1234', '아톰', 'www.naver.com');
 
 INSERT INTO mission (title, thumbnail, summary, url)
-VALUES ('루터회관 흡연 단속', 'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-smoking.png', '담배피다 걸린 행성이를 위한 벌금 계산 미션', 'https://github.com/develup-mission/java-smoking');
+VALUES ('루터회관 흡연 단속', 'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-smoking.png',
+        '담배피다 걸린 행성이를 위한 벌금 계산 미션', 'https://github.com/develup-mission/java-smoking');
 INSERT INTO mission (title, thumbnail, summary, url)
-VALUES ('java-guessing-number', 'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-guessing-number.png', '숫자를 맞춰보자', 'https://github.com/develup-mission/java-guessing-number');
+VALUES ('java-guessing-number',
+        'https://raw.githubusercontent.com/develup-mission/docs/main/image/java-guessing-number.png', '숫자를 맞춰보자',
+        'https://github.com/develup-mission/java-guessing-number');
 
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 1, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 2, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (1, 3, '라이언 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 1, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 2, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
-INSERT INTO solution (mission_id, member_id, title, description, url, status) VALUES (2, 3, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (1, 1, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (1, 2, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (1, 3, '라이언 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (2, 1, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (2, 2, '릴리 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+INSERT INTO solution (mission_id, member_id, title, description, url, status)
+VALUES (2, 3, '아톰 미션 제출합니다.', '안녕하세요. 잘 부탁 드립니다.', 'https://github.com/develup/mission/pull/1', 'COMPLETED');
+
+-- root-1
+-- ㄴ root-1-1
+-- ㄴ root-1-2
+-- root-2 (deleted, view o)
+-- ㄴ root-2-1
+-- ㄴ root-2-2 (deleted, view x)
+-- root-3 (deleted, view x)
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '1', NULL, NULL, '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '2', NULL, '2021-08-01 00:00:00', '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '3', NULL, '2021-08-01 00:00:00', '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '2-1', 2, NULL, '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '1-1', 1, NULL, '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '2-2', 2, '2021-08-01 00:00:00', '2021-08-01 00:00:00');
+INSERT INTO solution_comment (solution_id, member_id, content, parent_comment_id, deleted_at, created_at)
+VALUES (1, 1, '1-2', 1, NULL, '2021-08-01 00:00:00');
+
