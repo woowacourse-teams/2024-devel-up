d7a96992031489967e43e25fcf8a22289035e936
hanyoung <phanyoung@naver.com>
2024-08-14 11:29:22 +0900

    feat: 로그인 시에만 댓글창이 보이도록 처리

--- frontend/src/pages/SolutionDetailPage.tsx
+++ frontend/src/pages/SolutionDetailPage.tsx
@@ -3,14 +3,16 @@ import CommentList from '@/components/SolutionDetail/CommentList';
 import * as S from '@/components/SolutionDetail/SolutionDetail.styled';
 import usePathnameAt from '@/hooks/usePathnameAt';
 import CommentsMock from '@/components/SolutionDetail/CommentList/CommentsMock.json';
+import useUserInfo from '@/hooks/useUserInfo';
 
 export default function SolutionDetailPage() {
+  const { data: userInfo } = useUserInfo();
   const solutionId = Number(usePathnameAt(-1));
 
   return (
     <S.SolutionDetailPageContainer>
       <CommentList comments={CommentsMock} />
-      <CommentForm solutionId={solutionId} />
+      {userInfo && <CommentForm solutionId={solutionId} />}
     </S.SolutionDetailPageContainer>
   );
 }
