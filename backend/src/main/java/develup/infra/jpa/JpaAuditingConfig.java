4920cdbf93bb7f978257971a2c999d06625b1672
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-15 19:52:09 +0900

    refactor: JpaAuditingConfig /infra/jpa로 폴더명 변경

--- backend/src/main/java/develup/infra/jpa/JpaAuditingConfig.java
+++ backend/src/main/java/develup/infra/jpa/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package develup.infra.config;
+package develup.infra.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
