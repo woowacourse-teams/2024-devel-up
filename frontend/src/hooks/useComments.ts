fba805b8eb3c6b49d6a4c811ba7a08be10b65d73
hanyoung <phanyoung@naver.com>
2024-08-16 14:16:40 +0900

    chore: 불필요한 주석 삭제

--- frontend/src/hooks/useComments.ts
+++ frontend/src/hooks/useComments.ts
@@ -3,9 +3,6 @@ import { commentKeys } from './queries/keys';
 import { getComments } from '@/apis/commentAPI';
 
 export const useComments = (solutionId: number) => {
-  // TODO(@Ryan): api 나오면 mock 데이터를 사용하지 않고 api 연결할 예정입니다.
-  solutionId;
-
   return useSuspenseQuery({
     queryKey: commentKeys.all,
     queryFn: () => getComments(solutionId),
