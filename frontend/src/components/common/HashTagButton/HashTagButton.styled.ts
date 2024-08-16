985a55eeffae8e7c918b5758766bd8cab3ba02ef
bmj0053 <bmj05378@gmail.com>
2024-08-16 14:06:30 +0900

    fix: hashTagButton 충돌 해결

--- frontend/src/components/common/HashTagButton/HashTagButton.styled.ts
+++ frontend/src/components/common/HashTagButton/HashTagButton.styled.ts
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 
-export const Button = styled.div`
-  background-color: var(--primary-50);
+interface ButtonProps {
+  $isSelected: boolean;
+}
+
+export const Button = styled.button<ButtonProps>`
+  background-color: ${(props) => (props.$isSelected ? 'var(--primary-100)' : 'var(--primary-50)')};
   color: var(--black-color);
+  transition: 0.2s;
+
+  padding: 1rem 1.6rem;
+  border-radius: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
 
   font-size: 1.2rem;
-  font-weight: 500;
   font-family: inherit;
+  font-weight: 500;
 
-  padding: 1rem 1.6rem;
-  border-radius: 2rem;
+  &:hover {
+    background-color: ${(props) =>
+      props.$isSelected ? 'var(--primary-200)' : 'var(--primary-100)'};
+  }
 `;
