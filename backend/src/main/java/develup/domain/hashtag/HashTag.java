f624fcda14a3f821715e858ce117d7ee3ee48d37
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-16 13:35:37 +0900

    refactor: HashTag, Mission, MissionHashTag에 IdentifiableEntity 적용

--- backend/src/main/java/develup/domain/hashtag/HashTag.java
+++ backend/src/main/java/develup/domain/hashtag/HashTag.java
@@ -1,18 +1,11 @@
 package develup.domain.hashtag;
 
-import java.util.Objects;
+import develup.domain.IdentifiableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
-public class HashTag {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class HashTag extends IdentifiableEntity {
 
     @Column(nullable = false)
     private String name;
@@ -25,32 +18,11 @@ public class HashTag {
     }
 
     public HashTag(Long id, String name) {
-        this.id = id;
+        super(id);
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof HashTag hashTag)) {
-            return false;
-        }
-
-        return this.getId() != null && Objects.equals(getId(), hashTag.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
     }
 }
--- backend/src/main/java/develup/domain/mission/MissionHashTag.java
+++ backend/src/main/java/develup/domain/mission/MissionHashTag.java
@@ -1,20 +1,14 @@
 package develup.domain.mission;
 
+import develup.domain.IdentifiableEntity;
 import develup.domain.hashtag.HashTag;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class MissionHashTag {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class MissionHashTag extends IdentifiableEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
@@ -32,13 +26,9 @@ public class MissionHashTag {
     }
 
     public MissionHashTag(Long id, Mission mission, HashTag hashTag) {
-        this.id = id;
+        super(id);
         this.mission = mission;
         this.hashTag = hashTag;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public Mission getMission() {
