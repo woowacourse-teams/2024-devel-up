4623f5a02c8bb1de4f9de6a5d403348120ed1061
bmj0053 <bmj05378@gmail.com>
2024-08-18 22:46:54 +0900

    test: useMissions 필터링 기능 테스트 코드 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -5,10 +5,18 @@ import missions from './missions.json';
 import submittedSolutions from './SubmittedSolutions.json';
 
 export const handlers = [
-  http.get(`${BASE_URL.dev}${PATH.missionList}`, () => {
-    return HttpResponse.json({ data: missions });
+  http.get(`${BASE_URL.dev}${PATH.missionList}`, ({ request }) => {
+    const url = new URL(request.url);
+    const hashTag = url.searchParams.get('hashTag');
+    if (hashTag === 'all') {
+      return HttpResponse.json({ data: missions });
+    }
+
+    const filteredMissions = missions.filter((mission) => mission.hashTags[0].name === hashTag);
+
+    return HttpResponse.json({ data: filteredMissions });
   }),
-  
+
   http.get(`${BASE_URL.dev}${PATH.missionList}/:id`, ({ request }) => {
     const url = new URL(request.url);
     const id = Number(url.pathname.split('/').pop());
