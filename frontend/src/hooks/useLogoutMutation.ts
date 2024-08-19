41b5597b54c3788e0042e1e1ce10bdf2c2fbd021
brgndy <brgndy96@gmail.com>
2024-08-19 14:48:11 +0900

    feat: 로그아웃 관련 뮤테이션 생성

---
+++ frontend/src/hooks/useLogoutMutation.ts
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query';
+import { deleteLogout } from '@/apis/authAPI';
+
+const useLogoutMutation = () => {
+  const { mutate: userLogoutMutation } = useMutation({
+    mutationFn: deleteLogout,
+    onSuccess: () => {
+      window.location.reload();
+    },
+    onError: (error: Error) => {
+      console.error(error.message);
+    },
+  });
+
+  const handleUserLogout = async () => {
+    userLogoutMutation();
+  };
+
+  return { handleUserLogout };
+};
+
+export default useLogoutMutation;
