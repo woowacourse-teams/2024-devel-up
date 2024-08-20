4e2a1a88c3ca5373db5363a21f9f3a3ae88460ad
robinjoon <robin980108@naver.com>
2024-08-20 23:19:44 +0900

    fix: CORS 설정을 프로파일 별로 다르게 설정하도록 수정

--- backend/src/main/java/develup/api/config/WebConfig.java
+++ backend/src/main/java/develup/api/config/WebConfig.java
@@ -2,6 +2,7 @@ package develup.api.config;
 
 import java.util.List;
 import develup.api.auth.AuthArgumentResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${client-host}")
+    private String clientHost;
 
     private final AuthArgumentResolver authArgumentResolver;
 
@@ -19,9 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://3.38.11.109", "https://www.devel-up.co.kr",
-                        "https://devel-up.co.kr", "https://api.devel-up.co.kr", "https://dev.api.devel-up.co.kr",
-                        "https://dev.devel-up.co.kr", "https://www.dev.devel-up.co.kr")
+                .allowedOrigins(clientHost)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true);
     }
