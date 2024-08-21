4c7a028840c10a85af013fd5ac3066ffa2f735ef
brgndy <brgndy96@gmail.com>
2024-08-21 17:57:24 +0900

    chore: 이미지 repush

---
+++ frontend/src/pages/AboutPage/AboutPage.styled.ts
@@ -0,0 +1,17 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+export const ButtonWrapper = styled.div`
+  position: relative;
+`;
+
+export const Button = styled.button`
+  position: absolute;
+`;
