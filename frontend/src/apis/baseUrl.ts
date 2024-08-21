d6281f9ce8fc6313ab10b39a2a08f766040df66c
박한영(Ryan) <phanyoung@naver.com>
2024-08-21 14:01:50 +0900

    재배포 시 캐싱 초기화를 위해 content hash 추가 및 배포 환경에 따른 서버 url 분기 처리 (issue #367) (#369)
    * 로그아웃 뮤테이션 생성 및 제출시 로직 개선 (issue #306) (#327)
    
    * feat: 로그아웃 관련 뮤테이션 생성
    
    * refactor: useModal 리팩토링
    
    * refactor: 불필요한 주석 제거
    
    * fix: 미션 제출 후 새로고침 로직 개선
    
    * refactor: 대쉬보드 프로필 레이아웃 수정
    
    * refactor: description null값도 보낼수 있도록 수정
    
    * refactor: 미션명 검증 섹션 추가
    
    * fix: 린트 disable
    
    * refactor: 수정사항 반영
    
    * 해시태그 버튼 및 필터링 구현 (issue #303) (#332)
    
    * feat: 미션 리스트 필터링 기능 구현
    
    * refactor: 미션 호출 api의 url 분리 및 queryKey에 filter 추가
    
    * fix: hashTagButton 충돌 해결
    
    * feat: 전체 데이터 불러오는 'all'로 변경, HashTag 필터링 mock data 임시 구현
    
    * feat: 솔루션 필터링 적용, useSolutions를 useSolutionSummaries로 변경
    
    * design: 솔루션 리스트 디자인 수정
    
    * design: 미션리스트 디자인 수정
    
    * feat: api 배포에 따른 mock data 제거
    
    * test: useMissions 필터링 기능 테스트 코드 추가
    
    * design: 미션, 솔루션 리스트 렌더링 시 애니메이션 효과 적용
    
    * refactor: 'all'을 상수로 변경
    
    * refactor: useToggleHashTag 제거 및 해시태그 토글 상태 추가
    
    * chore: 개발 환경과 배포환경에 따른 백엔드 API 경로 수정 (#344)
    
    * fix: 대시보드 렌더링 분기처리 오류 수정 (#346)
    
    ---------
    
    Co-authored-by: JEON TAEHEON <brgndyy@gmail.com>
    Co-authored-by: Minji <121149171+chosim-dvlpr@users.noreply.github.com>

--- frontend/src/apis/baseUrl.ts
+++ frontend/src/apis/baseUrl.ts
@@ -1,4 +1,7 @@
 export const BASE_URL = {
   dev: 'https://dev.api.devel-up.co.kr',
-  prod: 'https://api.devel-up.co.kr',
+  prod:
+    process.env.NODE_ENV === 'production'
+      ? 'https://api.devel-up.co.kr'
+      : 'https://dev.api.devel-up.co.kr',
 } as const;
