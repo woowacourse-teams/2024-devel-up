99bc212f7b0e232df8923d9c9cae0096613f712a
JEON TAEHEON <brgndyy@gmail.com>
2024-08-23 00:57:01 +0900

    어바웃 페이지 SVG 파일 관련 오류수정 (issue #401) (#403)
    * refactor: 컨텐츠 배너 컨텐츠 변경 및 라우팅 연결
    
    * refactor: 이미지 svg로 변경

--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -2,14 +2,21 @@ import * as S from './MainPage.styled';
 import useMissions from '@/hooks/useMissions';
 import MissionList from '@/components/MissionList';
 import Carousel from '@/components/Carousel/Carousel';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/constants/routes';
 
 export default function MainPage() {
   const { data: missions } = useMissions();
+  const navigate = useNavigate();
+
+  const navigateToAboutPage = () => {
+    navigate(ROUTES.about);
+  };
 
   return (
     <S.MainPageContainer>
       <Carousel>
-        <S.BannerContent />
+        <S.BannerContent onClick={navigateToAboutPage} />
       </Carousel>
       <S.MissionListTitle>새로운 미션에 참여해 보세요!</S.MissionListTitle>
       <MissionList missions={missions} />
