f0aef6ef5ea375ddc7742bdc0c4c1156eb279c0c
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-05 17:12:37 +0900

    feat: 디스커션 레포지터리 작성

---
+++ backend/src/main/java/develup/domain/discussion/DiscussionRepository.java
@@ -0,0 +1,6 @@
+package develup.domain.discussion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
+}
