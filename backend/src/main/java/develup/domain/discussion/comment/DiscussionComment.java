7e4819ec3e0006760d15ee5eb60226d5a3739052
임수빈 <robin980108@naver.com>
2024-09-09 00:24:10 +0900

    feat: 디스커션 댓글 도메인 추가

---
+++ backend/src/main/java/develup/domain/discussion/comment/DiscussionComment.java
@@ -0,0 +1,80 @@
+package develup.domain.discussion.comment;
+
+import java.time.LocalDateTime;
+import develup.domain.CreatedAtAuditableEntity;
+import develup.domain.discussion.Discussion;
+import develup.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class DiscussionComment extends CreatedAtAuditableEntity {
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "discussion_id", nullable = false)
+    private Discussion discussion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private DiscussionComment parentComment;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public DiscussionComment(
+            String content,
+            Discussion discussion,
+            Member member,
+            DiscussionComment parentComment,
+            LocalDateTime deletedAt
+    ) {
+        this(null, content, discussion, member, parentComment, deletedAt);
+    }
+
+    public DiscussionComment(
+            Long id,
+            String content,
+            Discussion discussion,
+            Member member,
+            DiscussionComment parentComment,
+            LocalDateTime deletedAt
+    ) {
+        super(id);
+        this.content = content;
+        this.discussion = discussion;
+        this.member = member;
+        this.parentComment = parentComment;
+        this.deletedAt = deletedAt;
+    }
+
+    public static DiscussionComment create(String content, Discussion discussion, Member member) {
+        return new DiscussionComment(content, discussion, member, null, null);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Discussion getDiscussion() {
+        return discussion;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public DiscussionComment getParentComment() {
+        return parentComment;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+}
