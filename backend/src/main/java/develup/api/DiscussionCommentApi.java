5d8a3c16f778c8dbc392c999e8dd2a15ed39e77a
임수빈 <robin980108@naver.com>
2024-09-09 14:29:33 +0900

    feat: 디스커션 댓글 조회 api 추가

---
+++ backend/src/main/java/develup/api/DiscussionCommentApi.java
@@ -0,0 +1,28 @@
+package develup.api;
+
+import java.util.List;
+import develup.api.common.ApiResponse;
+import develup.application.discussion.comment.DiscussionCommentService;
+import develup.application.discussion.comment.GroupingDiscussionCommentResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "디스커션 댓글 API")
+public class DiscussionCommentApi {
+
+    private final DiscussionCommentService discussionCommentService;
+
+    public DiscussionCommentApi(DiscussionCommentService discussionCommentService) {
+        this.discussionCommentService = discussionCommentService;
+    }
+
+    @GetMapping("/discussions/{discussionId}/comment")
+    public ResponseEntity<ApiResponse<List<GroupingDiscussionCommentResponse>>> getGroupedComments(@PathVariable Long discussionId) {
+        var responses = discussionCommentService.getGroupingComments(discussionId);
+        return ResponseEntity.ok(new ApiResponse<>(responses));
+    }
+}
