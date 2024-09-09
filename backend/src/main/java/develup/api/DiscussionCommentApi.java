cab305e3673c3271ac2431cdb6ee2415ead329cc
임수빈 <robin980108@naver.com>
2024-09-09 17:13:28 +0900

    chore: swagger 설명 추가

--- backend/src/main/java/develup/api/DiscussionCommentApi.java
+++ backend/src/main/java/develup/api/DiscussionCommentApi.java
@@ -8,6 +8,7 @@ import develup.application.discussion.comment.DiscussionCommentCreateRequest;
 import develup.application.discussion.comment.DiscussionCommentResponse;
 import develup.application.discussion.comment.DiscussionCommentService;
 import develup.application.discussion.comment.GroupingDiscussionCommentResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,12 +28,14 @@ public class DiscussionCommentApi {
     }
 
     @GetMapping("/discussions/{discussionId}/comments")
+    @Operation(summary = "디스커션 댓글 목록 조회 API", description = "디스커션 댓글 목록을 조회합니다.")
     public ResponseEntity<ApiResponse<List<GroupingDiscussionCommentResponse>>> getGroupedComments(@PathVariable Long discussionId) {
         var responses = discussionCommentService.getGroupingComments(discussionId);
         return ResponseEntity.ok(new ApiResponse<>(responses));
     }
 
     @PostMapping("/discussions/{discussionId}/comments")
+    @Operation(summary = "디스커션 댓글 작성 API", description = "디스커션에 댓글을 작성합니다.")
     public ResponseEntity<ApiResponse<DiscussionCommentResponse>> createComment(
             @Auth Accessor accessor,
             @PathVariable Long discussionId,
