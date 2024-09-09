99c87f68a611f8eeb8bf3a02fbf4c3f128a8b394
임수빈 <robin980108@naver.com>
2024-09-09 17:16:53 +0900

    feat: 댓글 본문 빈 값 금지

--- backend/src/main/java/develup/application/discussion/comment/DiscussionCommentCreateRequest.java
+++ backend/src/main/java/develup/application/discussion/comment/DiscussionCommentCreateRequest.java
@@ -1,10 +1,12 @@
 package develup.application.discussion.comment;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.lang.Nullable;
 
 public record DiscussionCommentCreateRequest(
         @Nullable
         Long parentCommentId,
+        @NotBlank
         String content
 ) {
 }
