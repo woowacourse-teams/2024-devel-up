13d6133bc72f0e6733a9bfbe8151f3ab5c81c505
임수빈 <robin980108@naver.com>
2024-09-12 16:51:53 +0900

    fix: healthcheck 주기 변경

--- backend/scripts/healthcheck.sh
+++ backend/scripts/healthcheck.sh
@@ -1,12 +1,12 @@
-for i in {1..10}; do
+for i in {1..5}; do
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/health)
 echo "Attempt $i: Response Code $response"
 
-if [ "$i" -eq 10 ] && [ "$response" -ne 200 ]; then
+if [ "$i" -eq 5 ] && [ "$response" -ne 200 ]; then
 echo "Health check failed after 10 attempts."
 exit 1
 fi
 
-sleep 10
+sleep 5
 done
 echo "Health check passed."
