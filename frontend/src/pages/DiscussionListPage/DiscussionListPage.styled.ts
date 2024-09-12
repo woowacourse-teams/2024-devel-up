c31791e6852a46c6ddf566e71092e5b7a246c254
bmj0053 <bmj05378@gmail.com>
2024-09-13 00:13:22 +0900

    design: 디스커션 리스트 margin 추가

--- frontend/src/pages/DiscussionListPage/DiscussionListPage.styled.ts
+++ frontend/src/pages/DiscussionListPage/DiscussionListPage.styled.ts
@@ -4,7 +4,7 @@ export const DiscussionListPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4.5rem;
-  margin: 0 auto;
+  margin: 4.5rem auto 0;
   width: 100%;
   max-width: 100rem;
 `;
