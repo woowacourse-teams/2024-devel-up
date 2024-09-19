75966b10bb72713382e7377c031316e0b726b203
임수빈 <robin980108@naver.com>
2024-09-19 16:31:11 +0900

    스크립트 오타수정

--- backend/src/main/java/develup/api/HealthApi.java
+++ backend/src/main/java/develup/api/HealthApi.java
@@ -10,6 +10,8 @@ public class HealthApi {
 
     @GetMapping("/health")
     public ResponseEntity<ApiResponse<String>> health() {
+
+
         return ResponseEntity.status(200).body(new ApiResponse<>("up"));
     }
 }
