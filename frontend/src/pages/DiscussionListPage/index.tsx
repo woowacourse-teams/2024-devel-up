5c89019713672116077aec8e86f5a7b49b1fc423
bmj0053 <bmj05378@gmail.com>
2024-09-21 20:40:00 +0900

    feat: 필터링 기능 구현

--- frontend/src/pages/DiscussionListPage/index.tsx
+++ frontend/src/pages/DiscussionListPage/index.tsx
@@ -1,11 +1,41 @@
 import DiscussionList from '@/components/DiscussionList';
 import * as S from './DiscussionListPage.styled';
+import { useState } from 'react';
+import useDiscussions from '@/hooks/useDiscussions';
+import TagList from '@/components/common/TagList';
+import useHashTags from '@/hooks/useHashTags';
+import type { HashTag } from '@/types';
+import useMissions from '@/hooks/useMissions';
+
+interface SelectedMissionType {
+  id: number;
+  title: string;
+}
 
 export default function DiscussionListPage() {
+  const [selectedMission, setSelectedMission] = useState<SelectedMissionType | null>(null);
+  const [selectedHashTag, setSelectedHashTag] = useState<HashTag | null>(null);
+
+  const { data: discussions } = useDiscussions(selectedMission?.title, selectedHashTag?.name);
+  const { data: allHashTags } = useHashTags();
+  const { data: allMissions } = useMissions(selectedHashTag?.name); // TODO: 미션 필터링용 미션 리스트 API 나오면 변경 필요
+
   return (
     <S.DiscussionListPageContainer>
       <S.DiscussionListTitle>💬 Discussion</S.DiscussionListTitle>
-      <DiscussionList />
+      <TagList
+        tags={allMissions}
+        setSelectedTag={setSelectedMission}
+        selectedTag={selectedMission}
+        keyName="title"
+      />
+      <TagList
+        tags={allHashTags}
+        setSelectedTag={setSelectedHashTag}
+        selectedTag={selectedHashTag}
+        keyName="name"
+      />
+      <DiscussionList discussions={discussions} />
     </S.DiscussionListPageContainer>
   );
 }
