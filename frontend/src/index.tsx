9a71ebe85da4391e31ea940100ceda6a492a1310
bmj0053 <bmj05378@gmail.com>
2024-09-23 16:09:25 +0900

    fix: 디스커션 리스트 페이지 추가 및 index 페이지 import 정리

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,8 +12,6 @@ import * as Sentry from '@sentry/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import './styles/fonts.css';
-import DashboardDiscussionPage from './pages/DashboardPage/Discussion';
-import DashboardDiscussionCommentPage from './pages/DashboardPage/DiscussionComment';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,24 +30,40 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 });
 
-const MissionDetailPage = lazy(() => import('./pages/MissionDetailPage'));
+// 메인
 const MainPage = lazy(() => import('./pages/MainPage'));
-const MissionSubmitPage = lazy(() => import('./pages/MissionSubmitPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'));
 const UserProfilePage = lazy(() => import('./pages/UserProfilePage'));
 // const GuidePage = lazy(() => import('./pages/GuidePage'));
-const ErrorPage = lazy(() => import('./pages/ErrorPage'));
-const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'));
+
+// 미션
+const MissionDetailPage = lazy(() => import('./pages/MissionDetailPage'));
+const MissionSubmitPage = lazy(() => import('./pages/MissionSubmitPage'));
+const MissionListPage = lazy(() => import('./pages/MissionListPage'));
+
+// 풀이 (솔루션)
+const SolutionListPage = lazy(() => import('./pages/SolutionListPage'));
+const SolutionDetailPage = lazy(() => import('./pages/SolutionDetailPage'));
+
+// 디스커션
 const DiscussionDetailPage = lazy(() => import('./pages/DiscussionDetailPage'));
 const DiscussionSubmitPage = lazy(() => import('./pages/DiscussionSubmitPage'));
-const SolutionListPage = lazy(() => import('./pages/SolutionListPage'));
-const MissionListPage = lazy(() => import('./pages/MissionListPage'));
-const SolutionDetailPage = lazy(() => import('./pages/SolutionDetailPage'));
+const DiscussionListPage = lazy(() => import('./pages/DiscussionListPage'));
+
+// 대시보드
 const DashboardPage = lazy(() => import('./pages/DashboardPage'));
+const DashboardDiscussionPage = lazy(() => import('./pages/DashboardPage/Discussion'));
+const DashboardDiscussionCommentPage = lazy(
+  () => import('./pages/DashboardPage/DiscussionComment'),
+);
 const DashBoardMissionInProgressPage = lazy(
   () => import('./pages/DashboardPage/MissionInProgress'),
 );
 const MyCommentsPage = lazy(() => import('./pages/DashboardPage/MyComments'));
 const SubmittedSolutionList = lazy(() => import('./components/DashBoard/SubmittedSolutions'));
+
+// 기타
+const ErrorPage = lazy(() => import('./pages/ErrorPage'));
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -222,6 +236,16 @@ const routes = [
       <App>
         <Suspense fallback={<LoadingSpinner />}>
           <DiscussionSubmitPage />
+        </Suspense>
+      </App>
+    ),
+  },
+  {
+    path: ROUTES.discussions,
+    element: (
+      <App>
+        <Suspense fallback={<LoadingSpinner />}>
+          <DiscussionListPage />
         </Suspense>
       </App>
     ),
