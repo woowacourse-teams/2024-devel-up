edb509d29d39a27cb444ed6f847e0c7a9d09c24c
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-24 12:43:36 +0900

    refactor: ddl column 순서 수정

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,43 +1,43 @@
 CREATE TABLE member (
-    created_at TIMESTAMP(6) NOT NULL,
     id BIGINT AUTO_INCREMENT,
-    social_id BIGINT NOT NULL,
     email VARCHAR(255),
-    image_url VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
     provider VARCHAR(255) NOT NULL CHECK (provider IN ('GITHUB')),
+    social_id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP(6) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE mission (
     id BIGINT AUTO_INCREMENT,
-    summary VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    summary VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE discussion (
-    created_at TIMESTAMP(6) NOT NULL,
     id BIGINT AUTO_INCREMENT,
-    member_id BIGINT NOT NULL,
-    mission_id BIGINT,
-    content TEXT NOT NULL,
     title VARCHAR(255),
+    content TEXT NOT NULL,
+    mission_id BIGINT,
+    member_id BIGINT NOT NULL,
+    created_at TIMESTAMP(6) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (mission_id) REFERENCES mission(id)
 );
 
 CREATE TABLE discussion_comment (
-    created_at TIMESTAMP(6) NOT NULL,
-    deleted_at TIMESTAMP(6),
-    discussion_id BIGINT NOT NULL,
     id BIGINT AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    discussion_id BIGINT NOT NULL,
     member_id BIGINT NOT NULL,
     parent_comment_id BIGINT,
-    content TEXT NOT NULL,
+    deleted_at TIMESTAMP(6),
+    created_at TIMESTAMP(6) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (discussion_id) REFERENCES discussion(id),
     FOREIGN KEY (member_id) REFERENCES member(id),
@@ -59,36 +59,36 @@ CREATE TABLE discussion_hash_tag (
 );
 
 CREATE TABLE mission_hash_tag (
-    hash_tag_id BIGINT NOT NULL,
     id BIGINT AUTO_INCREMENT,
     mission_id BIGINT NOT NULL,
+    hash_tag_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (hash_tag_id) REFERENCES hash_tag(id),
     FOREIGN KEY (mission_id) REFERENCES mission(id)
 );
 
 CREATE TABLE solution (
-    created_at TIMESTAMP(6) NOT NULL,
     id BIGINT AUTO_INCREMENT,
-    member_id BIGINT NOT NULL,
     mission_id BIGINT NOT NULL,
-    description TEXT,
-    status VARCHAR(255) NOT NULL CHECK (status IN ('IN_PROGRESS','COMPLETED')),
+    member_id BIGINT NOT NULL,
     title VARCHAR(255),
+    description TEXT,
     url VARCHAR(255),
+    status VARCHAR(255) NOT NULL CHECK (status IN ('IN_PROGRESS','COMPLETED')),
+    created_at TIMESTAMP(6) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (mission_id) REFERENCES mission(id)
 );
 
 CREATE TABLE solution_comment (
-    created_at TIMESTAMP(6) NOT NULL,
-    deleted_at TIMESTAMP(6),
     id BIGINT AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    solution_id BIGINT NOT NULL,
     member_id BIGINT NOT NULL,
     parent_comment_id BIGINT,
-    solution_id BIGINT NOT NULL,
-    content TEXT NOT NULL,
+    deleted_at TIMESTAMP(6),
+    created_at TIMESTAMP(6) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (parent_comment_id) REFERENCES solution_comment(id),
