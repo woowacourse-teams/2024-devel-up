cd716a78a44ae08d48465ef5d28cf3698800c448
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-26 14:16:53 +0900

    대시보드 솔루션 및 디스커션 조회 시 createdAt 추가 (issue #531) (#533)
    * feat: 나의 디스커션 목록 조회 응답에 createdAt 추가
    
    * feat: 나의 솔루션 조회 응답에 createdAt 추가

--- backend/src/test/java/develup/api/DiscussionApiTest.java
+++ backend/src/test/java/develup/api/DiscussionApiTest.java
@@ -9,10 +9,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import develup.application.discussion.CreateDiscussionRequest;
 import develup.application.discussion.DiscussionResponse;
 import develup.application.discussion.SummarizedDiscussionResponse;
+import develup.application.hashtag.HashTagResponse;
+import develup.application.member.MemberResponse;
 import develup.domain.discussion.Discussion;
 import develup.domain.hashtag.HashTag;
 import develup.domain.member.Member;
@@ -103,9 +106,23 @@ public class DiscussionApiTest extends ApiTestSupport {
     @Test
     @DisplayName("나의 디스커션 목록을 조회한다.")
     void getMyDiscussions() throws Exception {
+        HashTag hashTag = HashTagTestData.defaultHashTag().withId(1L).build();
+        HashTagResponse hashTagResponse = HashTagResponse.from(hashTag);
+        List<HashTagResponse> hashTags = List.of(hashTagResponse);
+        Member member = MemberTestData.defaultMember().withId(1L).build();
+        MemberResponse memberResponse = MemberResponse.from(member);
+        LocalDateTime now = LocalDateTime.now();
+
         List<SummarizedDiscussionResponse> myDiscussions = List.of(
-                SummarizedDiscussionResponse.from(createDiscussion()),
-                SummarizedDiscussionResponse.from(createDiscussion())
+                new SummarizedDiscussionResponse(
+                        1L,
+                        "루터회관 흡연단속 구현에 대한 고찰",
+                        "루터회관 흡연단속",
+                        hashTags,
+                        memberResponse,
+                        100,
+                        now
+                )
         );
 
         BDDMockito.given(discussionReadService.getDiscussionsByMemberId(any()))
@@ -121,7 +138,8 @@ public class DiscussionApiTest extends ApiTestSupport {
                 .andExpect(jsonPath("$.data[0].hashTags[0].name", equalTo("JAVA")))
                 .andExpect(jsonPath("$.data[0].member.id", equalTo(1)))
                 .andExpect(jsonPath("$.data[0].member.name", equalTo("tester")))
-                .andExpect(jsonPath("$.data[0].commentCount", equalTo(100)));
+                .andExpect(jsonPath("$.data[0].commentCount", equalTo(100)))
+                .andExpect(jsonPath("$.data[0].createdAt").exists());
     }
 
     private Discussion createDiscussion() {
