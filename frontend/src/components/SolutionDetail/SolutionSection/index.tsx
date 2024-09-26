bc9a4045a6facac64ad82937d54fee0c87b026de
bmj0053 <bmj05378@gmail.com>
2024-09-26 11:32:09 +0900

    design: 버튼 옆 공간에도 클릭이 되는 버그 해결

--- frontend/src/components/SolutionDetail/SolutionSection/index.tsx
+++ frontend/src/components/SolutionDetail/SolutionSection/index.tsx
@@ -2,7 +2,6 @@ import * as S from './SolutionSection.styled';
 import type { Solution } from '@/types/solution';
 import Button from '@/components/common/Button/Button';
 import SolutionDetailHeader from './SolutionDetailHeader';
-import { Link } from 'react-router-dom';
 
 interface SolutionDetailProps {
   solution: Solution;
@@ -16,12 +15,12 @@ export default function SolutionSection({ solution }: SolutionDetailProps) {
       <S.SolutionDetailTitle>📝 Solutions</S.SolutionDetailTitle>
       <SolutionDetailHeader solution={solution} />
       <S.CodeViewButtonWrapper>
-        <Link to={url} target="_blank">
+        <S.CodeViewButtonLink to={url} target="_blank">
           <Button variant="default">
             <S.GithubIcon />
             코드 보러 가기
           </Button>
-        </Link>
+        </S.CodeViewButtonLink>
       </S.CodeViewButtonWrapper>
       <S.SolutionDescription>{description}</S.SolutionDescription>
     </section>
