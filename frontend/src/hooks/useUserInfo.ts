beef1c45ef34455dd3eab32e390b3d17f1aee293
brgndy <brgndy96@gmail.com>
2024-09-27 11:09:45 +0900

    fix: useQuery를 useSuspenseQuery로 수정

--- frontend/src/hooks/useUserInfo.ts
+++ frontend/src/hooks/useUserInfo.ts
@@ -1,14 +1,13 @@
 import { getUserInfo } from '@/apis/authAPI';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import type { UserInfo } from '../types/user';
 import { userKeys } from './queries/keys';
 
 const useUserInfo = () => {
-  return useQuery<UserInfo>({
+  return useSuspenseQuery<UserInfo>({
     queryKey: userKeys.info,
     queryFn: getUserInfo,
     retry: false,
-    throwOnError: false,
   });
 };
 
