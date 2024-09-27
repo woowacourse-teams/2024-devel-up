e4d7edc5ab86e5874b6845b3282484258ee72f3d
brgndy <brgndy96@gmail.com>
2024-09-27 12:21:01 +0900

    fix: a 태그로 변경

--- frontend/src/components/common/Error/ErrorFallback.tsx
+++ frontend/src/components/common/Error/ErrorFallback.tsx
@@ -1,7 +1,5 @@
 import { HTTP_ERROR_MESSAGE } from '@/constants/api';
-import Button from '../Button/Button';
 import * as S from './ErrorFallback.styled';
-import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/constants/routes';
 
 export interface ErrorFallbackProps {
@@ -17,18 +15,12 @@ const generateHTTPErrorMessage = (statusCode: number) => {
 const ErrorFallback = ({ statusCode }: ErrorFallbackProps) => {
   const { heading, body, button } = generateHTTPErrorMessage(statusCode);
 
-  const navigate = useNavigate();
-
-  const navigateToHome = () => {
-    navigate(ROUTES.main);
-  };
-
   return (
     <S.Container>
       <S.Wrapper>{heading}</S.Wrapper>
       <S.Wrapper>{body}</S.Wrapper>
       <S.Wrapper>
-        <Button onClick={navigateToHome}>{button}</Button>
+        <a href={ROUTES.main}>{button}</a>
       </S.Wrapper>
     </S.Container>
   );
