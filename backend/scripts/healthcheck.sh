461ce87033e0f6003bdc27a1ad45d563a97c085f
Kim Minjoo(김민주/리브) <131349867+Minjoo522@users.noreply.github.com>
2024-10-02 17:22:54 +0900

    롬복 의존성 추가 (issue #581) (#584)
    * chore: 오타 수정
    
    * chore: 롬복 의존성 추가
    
    ---------
    
    Co-authored-by: le2sky <leehaneul990623@gmail.com>

--- backend/scripts/healthcheck.sh
+++ backend/scripts/healthcheck.sh
@@ -3,7 +3,7 @@ response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/health)
 echo "Attempt $i: Response Code $response"
 
 if [ "$i" -eq 5 ] && [ "$response" -ne 200 ]; then
-echo "Health check failed after 10 attempts."
+echo "Health check failed after 5 attempts."
 exit 1
 fi
 
