fbe5320be11fa7a905c34b0e0acad48d2c180bbb
임수빈 <robin980108@naver.com>
2024-10-04 11:15:50 +0900

    chore: 안쓰는 메서드 삭제

--- backend/src/main/java/develup/domain/solution/comment/SolutionCommentCounts.java
+++ backend/src/main/java/develup/domain/solution/comment/SolutionCommentCounts.java
@@ -3,7 +3,6 @@ package develup.domain.solution.comment;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import develup.domain.solution.Solution;
 
 public class SolutionCommentCounts {
 
@@ -16,10 +15,6 @@ public class SolutionCommentCounts {
     private Map<Long, Long> toMap(List<SolutionCommentCount> solutionCommentCounts) {
         return solutionCommentCounts.stream()
                 .collect(Collectors.toMap(SolutionCommentCount::id, SolutionCommentCount::count));
-    }
-
-    public Long getCount(Solution solution) {
-        return solutionCommentCounts.getOrDefault(solution.getId(), 0L);
     }
 
     public Long getCount(Long solutionId) {
