45e0836de37114a12ca00b10ee723432d8e9f21f
le2sky <leehaneul990623@gmail.com>
2024-10-04 13:14:12 +0900

    test: 누락된 공백 검증 테스트 추가

--- backend/src/test/java/develup/domain/discussion/DiscussionTitleTest.java
+++ backend/src/test/java/develup/domain/discussion/DiscussionTitleTest.java
@@ -9,6 +9,14 @@ import org.junit.jupiter.api.Test;
 class DiscussionTitleTest {
 
     @Test
+    @DisplayName("디스커션 제목은 공백을 제외한 1글자 이상이어야 한다.")
+    void validateIsNotBlank() {
+        assertThatThrownBy(() -> new DiscussionTitle(" "))
+                .isInstanceOf(DevelupException.class)
+                .hasMessage("올바르지 않은 제목입니다.");
+    }
+
+    @Test
     @DisplayName("디스커션 제목은 50글자를 넘을 수 없다.")
     void validateLength() {
         assertThatThrownBy(() -> new DiscussionTitle("*".repeat(51)))
