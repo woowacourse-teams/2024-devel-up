af7e7494570a81bb708904e57ec157bcbd98f1c0
bmj0053 <bmj05378@gmail.com>
2024-10-08 00:21:41 +0900

    refactor: ConfirmButton props의 Confirm 핸들러 이름 변경

--- frontend/src/components/ModalContent/ConfirmButtons/index.tsx
+++ frontend/src/components/ModalContent/ConfirmButtons/index.tsx
@@ -4,19 +4,16 @@ import * as S from './ConfirmButtons.styled';
 
 interface ConfirmButtonsProps {
   handleModalClose: () => void;
-  handleSolutionDelete: () => void;
+  handleConfirm: () => void;
 }
 
-export default function ConfirmButtons({
-  handleModalClose,
-  handleSolutionDelete,
-}: ConfirmButtonsProps) {
+export default function ConfirmButtons({ handleModalClose, handleConfirm }: ConfirmButtonsProps) {
   return (
     <S.DeleteModalContainer>
       <ModalWrapper.Title>풀이를 삭제할까요?</ModalWrapper.Title>
       <S.ModalButtonWrapper>
         <Button onClick={handleModalClose}>취소</Button>
-        <Button variant="primary" onClick={handleSolutionDelete}>
+        <Button variant="primary" onClick={handleConfirm}>
           삭제
         </Button>
       </S.ModalButtonWrapper>
