037c473ea91312fffaeb3fde6df06a2cd5f03df4
bmj0053 <bmj05378@gmail.com>
2024-10-10 22:45:48 +0900

    refactor: fetching 중일 땐 ScrollToTopButton 보이지 않도록 추가

--- frontend/src/components/common/ScrollToTopButton/index.tsx
+++ frontend/src/components/common/ScrollToTopButton/index.tsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import UpArrow from '@/assets/images/upArrow.svg';
 import * as S from './ScrollToTopButton.styled';
+import { useIsFetching } from '@tanstack/react-query';
 
 export function ScrollToTopButton() {
   const { pathname } = useLocation();
+  const isFetching = useIsFetching();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -13,6 +15,10 @@ export function ScrollToTopButton() {
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+
+  if (isFetching > 0) {
+    return null;
+  }
 
   return (
     <S.ScrollButton onClick={handleScrollToTop}>
