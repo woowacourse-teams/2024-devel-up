3766f372288a5b96f4fe2cfd72c6c83788fea960
brgndy <brgndy96@gmail.com>
2024-10-10 18:51:19 +0900

    refactor: 어바웃페이지 존재하던 버튼 삭제

--- frontend/src/pages/AboutPage/AboutPage.tsx
+++ frontend/src/pages/AboutPage/AboutPage.tsx
@@ -1,51 +1,13 @@
 import * as S from './AboutPage.styled';
-import Button from '@/components/common/Button/Button';
-import { useNavigate } from 'react-router-dom';
-import { ROUTES } from '@/constants/routes';
 
 export default function AboutPage() {
-  const navigate = useNavigate();
-
-  const navigateToMissionList = () => {
-    navigate(ROUTES.missionList);
-  };
-
-  const navigateToMain = () => {
-    navigate(ROUTES.main);
-  };
-
   return (
     <S.Container>
       <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-3.webp" />
       <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-4.webp" />
       <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-5.webp" />
-      <S.ButtonWrapper>
-        <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-6.webp" />
-        <Button
-          variant="primary"
-          style={{
-            margin: '0 auto',
-          }}
-          onClick={navigateToMissionList}
-        >
-          풀 수 있는 미션 둘러보기
-        </Button>
-      </S.ButtonWrapper>
-
-      <S.ButtonWrapper>
-        <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-7.webp" />
-        <Button
-          variant="primary"
-          style={{
-            position: 'absolute',
-            bottom: '20rem',
-            left: '45%',
-          }}
-          onClick={navigateToMain}
-        >
-          지금 바로 Devel Up 시작하기
-        </Button>
-      </S.ButtonWrapper>
+      <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-6.webp" />
+      <S.Image src="https://dp71rnme1p14w.cloudfront.net/about-7.webp" />
     </S.Container>
   );
 }
--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -2,16 +2,10 @@ import * as S from './MainPage.styled';
 import useMissions from '@/hooks/useMissions';
 import MissionList from '@/components/MissionList';
 import { useNavigate } from 'react-router-dom';
-import { ROUTES } from '@/constants/routes';
 import AboutPage from '../AboutPage/AboutPage';
 
 export default function MainPage() {
   const { data: missions } = useMissions();
-  const navigate = useNavigate();
-
-  const navigateToAboutPage = () => {
-    navigate(ROUTES.about);
-  };
 
   return (
     <S.MainPageContainer>
