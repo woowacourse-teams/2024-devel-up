926dc6b3d5e224053cabffe95d81da4266fcbe92
brgndy <brgndy96@gmail.com>
2024-10-10 22:32:12 +0900

    refactor: 댓글 갯수 제거

--- frontend/src/components/DashBoard/MyComments/MyComment.tsx
+++ frontend/src/components/DashBoard/MyComments/MyComment.tsx
@@ -1,6 +1,6 @@
 import { formatDateString } from '@/utils/formatDateString';
 import * as S from './MyComments.styled';
-import CommentIcon from '@/assets/images/comment-count.svg';
+// import CommentIcon from '@/assets/images/comment-count.svg';
 
 interface MyCommentProps {
   contentId: number;
@@ -16,7 +16,7 @@ export default function MyComment({
   content,
   createdAt,
   contentTitle,
-  contentCommentCount,
+  // contentCommentCount,
   type,
 }: MyCommentProps) {
   const commentDate = formatDateString(createdAt);
@@ -29,10 +29,10 @@ export default function MyComment({
         <S.SubText>{commentDate}</S.SubText>
         <S.SubText>{contentTitle}</S.SubText>
       </S.TextWrapper>
-      <S.CommentCountWrapper>
+      {/* <S.CommentCountWrapper>
         <CommentIcon width={14} height={14} />
         <S.CommentCountText>{contentCommentCount}</S.CommentCountText>
-      </S.CommentCountWrapper>
+      </S.CommentCountWrapper> */}
     </S.CommentWrapper>
   );
 }
