2386bbe0b1450db3aa5a4c1a91aa5f05f7dbf6aa
JEON TAEHEON <brgndyy@gmail.com>
2024-10-11 11:10:13 +0900

    각종 수정 사항 반영 (feat#650) (#651)
    * refactor: 커스텀 뮤테이션 인자 변경
    
    * Merge branch 'dev' of https://github.com/woowacourse-teams/2024-devel-up into dev
    
    * refactor: 수정사항 반영

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -6,7 +6,12 @@ export default function Footer() {
       <S.TextWrapper>
         <S.Text>DevelUp</S.Text>
         <S.LinkTextWrapper>
-          <S.EmailText>develupteam@gmail.com</S.EmailText>
+          <S.LinkText
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfkgaVvmFSrrQ3TSO0qL00ZE98jog_eXK-zrUXtvBulO--dkA/viewform"
+            target={'_blank'}
+          >
+            데벨업에게 피드백 하기
+          </S.LinkText>
           <S.LinkText href="https://velog.io/@teamdevelup/posts" target={'_blank'}>
             개발 이야기
           </S.LinkText>
