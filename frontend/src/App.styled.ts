2354ff3bb3a9c8034b0effc0d0ddcbba0106609b
bmj0053 <bmj05378@gmail.com>
2024-10-15 18:00:29 +0900

    feat: 본문으로 바로가기 추가

---
+++ frontend/src/App.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SkipTag = styled.a`
+  position: absolute;
+  width: 0.1rem;
+  height: 0.1rem;
+  padding: 0;
+  margin: -0.1rem;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+`;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import { GlobalLayout } from './styles/GlobalLayout';
 import type { PropsWithChildren } from 'react';
 import Footer from './components/Footer';
 import { ScrollToTopButton } from './components/common/ScrollToTopButton';
+import * as S from './App.styled';
 
 export default function App({ children }: PropsWithChildren) {
   return (
     <>
       <GlobalLayout>
+        <S.SkipTag href="#main-content" className="visually-hidden">
+          본문으로 바로가기
+        </S.SkipTag>
         <Header />
-        {children}
+        <div id="main-content">{children}</div>
       </GlobalLayout>
       <Footer />
       <ScrollToTopButton />
