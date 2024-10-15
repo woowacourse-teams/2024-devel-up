5ed2c21a4239c1e0abb62bff4df6ff9b98486613
le2sky <leehaneul990623@gmail.com>
2024-10-15 23:23:35 +0900

    feat: 사용자가 작성한 디스커션 댓글 조회 페이지네이션 쿼리 구현

--- backend/src/main/java/develup/domain/discussion/comment/DiscussionCommentRepositoryCustom.java
+++ backend/src/main/java/develup/domain/discussion/comment/DiscussionCommentRepositoryCustom.java
@@ -6,8 +6,12 @@ import static develup.domain.member.QMember.member;
 
 import java.util.List;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -37,6 +41,39 @@ public class DiscussionCommentRepositoryCustom {
                 .join(discussionComment.member)
                 .where(discussionComment.member.id.eq(memberId).and(discussionComment.deletedAt.isNull()))
                 .orderBy(discussionComment.createdAt.desc())
+                .fetch();
+    }
+
+    public Page<MyDiscussionComment> findPageMyDiscussionCommentOrderByCreatedAtDesc(Long memberId, Pageable pageRequest) {
+        long offset = pageRequest.getOffset();
+        int limit = pageRequest.getPageSize();
+        JPAQuery<Long> countQuery = getMemberDiscussionCommentCountQuery(memberId);
+        List<MyDiscussionComment> data = fetchMemberDiscussionComments(memberId, offset, limit);
+
+        return PageableExecutionUtils.getPage(data, pageRequest, countQuery::fetchOne);
+    }
+
+    private JPAQuery<Long> getMemberDiscussionCommentCountQuery(Long memberId) {
+        return queryFactory.select(discussionComment.count())
+                .from(discussionComment)
+                .where(discussionComment.member.id.eq(memberId).and(discussionComment.deletedAt.isNull()));
+    }
+
+    private List<MyDiscussionComment> fetchMemberDiscussionComments(Long memberId, long offset, int limit) {
+        return queryFactory.select(Projections.constructor(MyDiscussionComment.class,
+                        discussionComment.id,
+                        discussionComment.discussion.id,
+                        discussionComment.content,
+                        discussionComment.createdAt,
+                        discussion.title.value
+                ))
+                .from(discussionComment)
+                .join(discussionComment.discussion, discussion)
+                .join(discussionComment.member)
+                .where(discussionComment.member.id.eq(memberId).and(discussionComment.deletedAt.isNull()))
+                .orderBy(discussionComment.createdAt.desc())
+                .offset(offset)
+                .limit(limit)
                 .fetch();
     }
 }
