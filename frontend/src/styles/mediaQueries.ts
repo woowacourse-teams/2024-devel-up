9b3800b8a412b31de5f7b43fc2e9a69f169b7a3e
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2024-10-16 13:04:32 +0900

    웹 페이지 접근성 헤더 적용 및 mediaQueries 추가 (issue #675) (#696)
    * feat: 본문으로 바로가기 추가
    
    * design: mediaQueries 추가 및 전체 디자인 레이아웃 조정
    - mediaQueries 추가
    - Header 및 Footer min-width, width 기본 조정
    - AboutPage 이미지 대체 및 Rocket Image 추가
    
    * design: skip button 포커스 시 화면에 보이도록 수정
    
    ---------
    
    Co-authored-by: JEON TAEHEON <brgndyy@gmail.com>

---
+++ frontend/src/styles/mediaQueries.ts
@@ -0,0 +1,25 @@
+import { css, type CSSObject, type Interpolation } from 'styled-components';
+
+export type Breakpoints = 'small' | 'large';
+
+export const breakpoints: Record<Breakpoints, string> = {
+  small: '@media (max-width: 480px)',
+  large: '@media (min-width: 481px)',
+};
+
+const media = Object.entries(breakpoints).reduce((acc, [key, value]) => {
+  return {
+    ...acc,
+    [key]: (
+      first: CSSObject | TemplateStringsArray,
+      ...interpolations: Interpolation<object>[]
+    ) => css`
+      ${value} {
+        ${css(first, ...interpolations)}
+      }
+    `,
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}, {}) as Record<Breakpoints, any>;
+
+export default media;
