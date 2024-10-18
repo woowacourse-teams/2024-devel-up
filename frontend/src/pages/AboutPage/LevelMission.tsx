18ce4e736ea960b6962b57e79f5ce0260df49298
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2024-10-18 17:00:27 +0900

    랜딩 페이지 이미지 대체 및 헤더, 랜딩 웹 접근성 추가 (issue #697) (#706)
    * feat: Rocket 영역 분리 및 LevelMission 영역 추가
    
    * feat: DiscussionSpace 영역 추가
    
    * feat: LevelMission 영역 수정 및 이미지 대체
    
    * refactor: mediaQueries에 landingMedium 사이즈 추가
    
    * feat: Solution 영역 구현 및 로켓 애니메이션 추가
    
    * feat: DownArrow 추가 및 디자인 수정
    
    * feat: useScrollComponent 구현
    - 스크롤 시 컴포넌트가 화면에 보이도록 구현
    
    * refactor: AboutPage 공통 디자인 속성 정리
    - levelMission 이미지 수정
    
    * fix: rocket 이미지 크기 에러 해결
    
    * refactor: AboutPage semantic tag 개선 및 소개 텍스트 스타일 코드 수정
    
    * feat: DownArrow 엔터 시 컴포넌트 이동 기능 추가
    
    * feat: 헤더 웹 접근성 추가 (로그인)
    
    * fix: 스타일 컴포넌트 isVisible props prefix $ 추가
    
    * fix: DownArrow 반복 클릭 시 발생하는 인덱스 오류 해결
    
    * design: 로켓이미지 breakpoint 수정

---
+++ frontend/src/pages/AboutPage/LevelMission.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as S from './AboutPage.styled';
+
+interface LevelMissionProps {
+  ref?: React.Ref<HTMLOptionElement>;
+  isVisible: boolean;
+}
+
+const LevelMission = React.forwardRef<HTMLOptionElement, LevelMissionProps>(
+  ({ isVisible }, ref) => {
+    return (
+      <S.ComponentContainer $isVisible={isVisible} ref={ref}>
+        <S.Wrapper>
+          <S.Bold>🎯 레벨 별 실전 문제 제공</S.Bold>
+          <S.ImgLeftPadding>
+            <S.LevelMissionImg />
+          </S.ImgLeftPadding>
+          <S.TextAlignCenterWrapper>
+            <S.MediumText>
+              <S.MediumBold>미션</S.MediumBold>을 직접 풀며
+              <br />
+              <S.MediumBold>프로그래밍 실력</S.MediumBold>을 진단할 수 있어요.
+            </S.MediumText>
+          </S.TextAlignCenterWrapper>
+        </S.Wrapper>
+      </S.ComponentContainer>
+    );
+  },
+);
+
+LevelMission.displayName = 'LevelMission';
+
+export default LevelMission;
