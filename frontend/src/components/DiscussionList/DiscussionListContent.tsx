7b5cc54d7897c098421559e16bf89a88cab0743a
박한영(Ryan) <phanyoung@naver.com>
2024-10-24 21:59:36 +0900

    카드 정렬 및 디스커션 서스펜스 처리 (#750)

--- frontend/src/components/DiscussionList/DiscussionListContent.tsx
+++ frontend/src/components/DiscussionList/DiscussionListContent.tsx
@@ -2,13 +2,29 @@ import * as S from './DiscussionList.styled';
 import DiscussionListItem from './DiscussionListItem';
 import { Link } from 'react-router-dom';
 import NoContentWithoutButton from '../common/NoContent/NoContentWithoutButton';
-import type { Discussion } from '@/types';
+import useDiscussions from '@/hooks/useDiscussions';
+import { HASHTAGS } from '@/constants/hashTags';
 
 interface DiscussionListContentProps {
-  discussions: Discussion[];
+  selectedMission?: string;
+  selectedHashTag?: string;
+  page: number;
+  onPageInfoUpdate?: (totalPage: number) => void;
 }
 
-export default function DiscussionListContent({ discussions }: DiscussionListContentProps) {
+export default function DiscussionListContent({
+  selectedMission,
+  selectedHashTag,
+  page,
+  onPageInfoUpdate,
+}: DiscussionListContentProps) {
+  const { discussions } = useDiscussions({
+    mission: selectedMission ?? HASHTAGS.all,
+    hashTag: selectedHashTag ?? HASHTAGS.all,
+    page,
+    onPageInfoUpdate,
+  });
+
   return (
     <S.ContentContainer>
       {discussions.length ? (
