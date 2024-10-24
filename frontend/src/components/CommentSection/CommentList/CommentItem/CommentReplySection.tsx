92d4f45d80f506e16bdf1b46aaa05fd308a316af
hanyoung <phanyoung@naver.com>
2024-10-24 11:35:27 +0900

    feat: QA 사항 반영

--- frontend/src/components/CommentSection/CommentList/CommentItem/CommentReplySection.tsx
+++ frontend/src/components/CommentSection/CommentList/CommentItem/CommentReplySection.tsx
@@ -44,7 +44,7 @@ export default function CommentReplySection({
 
   return (
     <S.CommentReplySectionContainer>
-      {isLoggedIn && !isParentDeleted && (
+      {!isLoggedIn && !isParentDeleted && (
         <>
           <S.ReplyWriteButton onClick={toggleReplyFormOpen}>답글</S.ReplyWriteButton>
         </>
--- frontend/src/components/DiscussionDetail/DiscussionDetail.styled.ts
+++ frontend/src/components/DiscussionDetail/DiscussionDetail.styled.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const DiscussionDetailHeaderContainer = styled.div`
-  width: 100rem;
-
   display: flex;
   flex-direction: column;
   position: relative;
